f8d6035dd850f2f63afffdf6c642673865e661ae
Phil Hagelberg <technomancy@gmail.com>
2009-11-02 21:22:57 -0800

    Implement compile command.

--- src/leiningen/compile.clj
+++ src/leiningen/compile.clj
@@ -1,7 +1,13 @@
 (ns leiningen.compile
+  (:use [clojure.contrib.java-utils :only [file]]
+        [clojure.contrib.find-namespaces :only [find-namespaces-in-dir]])
   (:refer-clojure :exclude [compile]))
 
-;; accept either a list of namespaces or spider the project root for .clj files
-;; create classes/ directory
-
-(defn compile [project])
+(defn compile
+  "Compile the namespaces specified in build.clj or all namespaces in src/
+  if none are provided."
+  [project]
+  (.mkdir (file (:root project) "classes"))
+  (doseq [n (or (:namespaces project)
+                (find-namespaces-in-dir (file (:root project) "src")))]
+    (compile n)))
