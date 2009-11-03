93bcde9126123507de446e8095e4363373d8ac16
Phil Hagelberg <technomancy@gmail.com>
2009-11-02 21:22:11 -0800

    Move to leiningen.core.

--- src/leiningen.clj
+++
@@ -1,9 +0,0 @@
-(ns leiningen)
-
-(defmacro defproject [project-name & args]
-  `(def ~project-name (assoc (apply hash-map (quote ~args))
-                        :name ~(name project-name)
-                        :root ~(.getParent (java.io.File. *file*)))))
-
-(defn read-project []
-  @(load-file "build.clj"))
--- src/leiningen/core.clj
+++ src/leiningen/core.clj
@@ -1,8 +1,23 @@
 (ns leiningen.core
-  (:require [leiningen deps test compile jar uberjar help])
+  (:require [leiningen deps test compile jar uberjar help]
+            [clojure.contrib.with-ns])
   (:gen-class))
+
+(declare project)
+
+(defmacro defproject [project-name & args]
+  `(do (def ~project-name (assoc (apply hash-map (quote ~args))
+                            :name ~(name project-name)
+                            :root ~(.getParent (java.io.File. *file*))))
+       (let [project# ~project-name]
+         (with-ns 'leiningen.core
+           (def '~'project project#)))))
+
+(defn read-project []
+  (load-file "build.clj")
+  project)
 
 (defn -main [command & args]
   (let [action (ns-resolve (symbol (str "leiningen." command))
                            (symbol command))]
-    (apply action (leiningen/read-project) args)))
+    (apply action (read-project) args)))
