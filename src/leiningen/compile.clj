c27e16e74401bfcdc90a9a0f836d02a4052c784c
Phil Hagelberg <technomancy@gmail.com>
2009-11-11 21:23:16 -0800

    Don't need to bind *compile-path* in compile.

--- src/leiningen/compile.clj
+++ src/leiningen/compile.clj
@@ -7,11 +7,8 @@
   "Compile the namespaces specified in build.clj or all namespaces in src/
   if none are provided."
   [project]
-  ;; TODO: not sure why this is necessary
-  (binding [*compile-path* "classes/"]
-    (.mkdir (file (:root project) "classes"))
-    (doseq [n (or (:namespaces project)
-                  (find-namespaces-in-dir (file (:root project) "src")))]
-      (println "Compiling" n)
-      ;; TODO: check to see if bytecode is older than source
-      (clojure.core/compile n))))
+  (doseq [n (or (:namespaces project)
+                (find-namespaces-in-dir (file (:root project) "src")))]
+    (println "Compiling" n)
+    ;; TODO: check to see if bytecode is older than source
+    (clojure.core/compile n)))
