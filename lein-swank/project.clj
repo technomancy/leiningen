b58d98f2c7b103c05b3211840a42c479d96e6e25
Phil Hagelberg <technomancy@gmail.com>
2009-11-20 14:11:23 -0800

    Update lein-swank.

--- lein-swank/project.clj
+++ lein-swank/project.clj
@@ -1,3 +1,3 @@
-(defproject lein-swank "0.5.0-SNAPSHOT"
-  :dependencies [[leiningen "0.5.0-SNAPSHOT"]
+(defproject lein-swank "1.0.0-SNAPSHOT"
+  :dependencies [[leiningen "1.0.0-SNAPSHOT"]
                  [org.clojure/swank-clojure "1.0"]])
