c8ce3cd014c8ab3732af4fc90ee3ee9260f3abed
Alex Osborne <ato@meshy.org>
2009-11-22 10:15:17 +1100

    When creating a standalone jar don't unpack deps.
    This means we don't have to unpack everything into classes/ which was
    causing people to accidentally create an uberjar when they did
    "lein jar".  We also merge plexus components.xml files, this means
    that uberjaring projects that embed Maven (like Leiningen itself)
    now works reliably.

--- src/leiningen/uberjar.clj
+++ src/leiningen/uberjar.clj
@@ -1,28 +1,64 @@
 (ns leiningen.uberjar
   "Create a jar containing the compiled code, source, and all dependencies."
-  (:use [clojure.contrib.java-utils :only [file]]
+  (:require [clojure.xml :as xml])
+  (:use [clojure.zip :only [xml-zip]]
+        [clojure.contrib.java-utils :only [file]]
         [clojure.contrib.duck-streams :only [copy]]
+        [clojure.contrib.zip-filter.xml :only [xml-> tag=]]
         [leiningen.deps :only [deps]]
         [leiningen.jar :only [jar]])
-  (:import [java.util.zip ZipFile]
-           [java.io File FileOutputStream]))
+  (:import [java.util.zip ZipFile ZipOutputStream ZipEntry]
+           [java.io File FileOutputStream PrintWriter]))
 
-(defn unzip
-  "Unzip zipfile into the target directory."
-  [zipfile target]
-  (with-open [zipfile (ZipFile. zipfile)]
-    (let [entries (enumeration-seq (.entries zipfile))]
-      (doseq [file (remove (memfn isDirectory) entries)]
-        (.mkdirs (.getParentFile (java.io.File. (str target (.getName file)))))
-        (with-open [out (FileOutputStream. (str target (.getName file)))]
-          (copy (.getInputStream zipfile file) out))))))
+(defn read-components [zipfile]
+  (when-let [entry (.getEntry zipfile "META-INF/plexus/components.xml")]
+    (-> (xml-> (xml-zip (xml/parse (.getInputStream zipfile entry)))
+               (tag= :components))
+        first first :content)))
+
+(defn copy-entries
+  "Copies the entries of ZipFile in to the ZipOutputStream out, skipping
+  the entries which satisfy skip-pred. Returns the names of the
+  entries copied."
+  [in out & [skip-pred]]
+  (for [file (enumeration-seq (.entries in))
+        :when (not (skip-pred file))]
+    (do
+      (.putNextEntry out file)
+      (copy (.getInputStream in file) out)
+      (.closeEntry out)
+      (.getName file))))
+
+;; we have to keep track of every entry we've copied so that we can
+;; skip duplicates.  We also collect together all the plexus components so
+;; that we can merge them.
+(defn include-dep [out [skip-set components] dep]
+  (println "Including" (.getName dep))
+  (with-open [zipfile (ZipFile. dep)]
+    [(into skip-set (copy-entries zipfile out #(skip-set (.getName %))))
+     (concat components (read-components zipfile))]))
 
 (defn uberjar
   "Create a jar like the jar task, but including the contents of each of
-the dependency jars. Suitable for standalone distribution."
+  the dependency jars. Suitable for standalone distribution."
   [project & args]
-  (doseq [dep (file-seq (file (:root project) "lib"))
-          :when (.endsWith (.getName dep) ".jar")]
-    (println "Unpacking" (.getName dep))
-    (unzip dep *compile-path*))
-  (jar project (str (:name project) "-standalone.jar")))
+  (jar project)
+  (with-open [out (-> (file (:root project)
+                            (str (:name project) "-standalone.jar"))
+                      (FileOutputStream.) (ZipOutputStream.))]
+    (let [deps (->> (file-seq (file (:root project) "lib"))
+                    (filter #(.endsWith (.getName %) ".jar"))
+                    (cons (file (:root project) (str (:name project) ".jar"))))
+          [_ components] (reduce (partial include-dep out)
+                                 [#{"META-INF/plexus/components.xml"} nil]
+                                 deps)]
+      (when-not (empty? components)
+        (.putNextEntry out (ZipEntry. "META-INF/plexus/components.xml"))
+        (binding [*out* (PrintWriter. out)]
+          (xml/emit {:tag :component-set
+                     :content
+                     [{:tag :components
+                       :content
+                       components}]})
+          (.flush *out*))
+        (.closeEntry out)))))
