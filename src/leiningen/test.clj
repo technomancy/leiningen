7ca03af258cd73d0db99026dc1e11075546b62f5
Dan Larkin <dan@danlarkin.org>
2009-11-21 22:18:35 -0500

    lein test should aggregate all test failures/passes etc and display a summary at the end of all the tests

--- src/leiningen/test.clj
+++ src/leiningen/test.clj
@@ -14,12 +14,30 @@
 (defn merge-predicates [preds]
   (fn [t] (every? #(% t) preds)))
 
+(let [orig-report report
+      aggregates (ref [])]
+  (defn lein-report [event]
+    (when (= (:type event) :summary)
+      (dosync (commute aggregates conj event)))
+    (orig-report event))
+
+  (defn super-summary []
+    (with-test-out
+      (println "\n\n--------------------\nTotal:"))
+    (orig-report (apply merge-with (fn [a b]
+                                     (if (number? a)
+                                       (+ a b)
+                                       a))
+                        @aggregates))))
+
 (defn run-matching [project preds]
   (binding [test-var (partial test-var-matching (merge-predicates preds))]
     (doseq [n (find-namespaces-in-dir
                (file (:root project) "test"))]
       (require n)
-      (run-tests n))))
+      (binding [report lein-report]
+        (run-tests n)))
+    (super-summary)))
 
 (defn test
   "Run the projects tests. Accept a list of predicates called with each test
