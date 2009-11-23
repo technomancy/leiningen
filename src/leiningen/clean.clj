e024e15c79ecf5c39cd4621803b5fda59a3ef3e6
Dan Larkin <dan@danlarkin.org>
2009-11-22 23:34:09 -0500

    improve "clean" command: Don't delete lib/ and classes/, just empty them. Also delete -standalone jar and pom-generated.xml

--- src/leiningen/clean.clj
+++ src/leiningen/clean.clj
@@ -3,9 +3,20 @@
   (:use [clojure.contrib.java-utils :only [file delete-file
                                            delete-file-recursively]]))
 
+(defn empty-directory
+  "Recursively delete all the files in f, but not f itself.
+Raise an exception if any deletion fails unless silently is true."
+  [f & [silently]]
+  (let [f (file f)]
+    (when (.isDirectory f)
+      (doseq [child (.listFiles f)]
+        (delete-file-recursively child silently)))))
+
 (defn clean [project & args]
+  (println "Cleaning up")
   (delete-file (str (:root project) "/" (:name project) ".jar") true)
+  (delete-file (str (:root project) "/" (:name project) "-standalone.jar") true)
+  (delete-file (str (:root project) "/pom-generated.xml") true)
   (doseq [d [(:compile-path project)
              (str (:root project) "/lib")]]
-    (println "Cleaning " d)
-    (delete-file-recursively (file d) true)))
+    (empty-directory (file d) true)))
