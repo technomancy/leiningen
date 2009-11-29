15cbad9dafa3b7f86b321d48d88fccfaa70c1193
Phil Hagelberg <technomancy@gmail.com>
2009-11-28 20:38:24 -0800

    Disable help summary output until Clojure bug #130 is fixed. =(

--- src/leiningen/help.clj
+++ src/leiningen/help.clj
@@ -5,14 +5,17 @@
 (def tasks (set (filter #(re-find #"^leiningen\.(?!core)" (name %))
                         (find-namespaces-on-classpath))))
 
-;; affected by clojure ticket #130: bug of AOT'd namespaces losing metadata
-(defn help-for [task]
+(defn help-for
+  "Help for a task is stored in its docstring, or if that's not present,
+  in its namespace."
+  [task]
   (let [task-ns (symbol (str "leiningen." task))
         _ (require task-ns)
         task (ns-resolve task-ns (symbol task))]
     (or (:doc (meta task))
         (:doc (meta (find-ns task-ns))))))
 
+;; affected by clojure ticket #130: bug of AOT'd namespaces losing metadata
 (defn help-summary-for [task-ns]
   (require task-ns)
   (let [task-name (last (.split (name task-ns) "\\."))]
@@ -25,6 +28,7 @@
     (do (println "Leiningen is a build tool for Clojure.\n")
         (println "Several tasks are available:")
         (doseq [task-ns tasks]
-          (println (help-summary-for task-ns)))
+          ;; (println (help-summary-for task-ns))
+          (println " " (last (.split (name task-ns) "\\."))))
         (println "\nRun lein help $TASK for details.")
         (println "See http://github.com/technomancy/leiningen as well."))))
