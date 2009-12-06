da41a91e0557d8b5c6657fe57516f20d6cc2535b
Phil Hagelberg <technomancy@gmail.com>
2009-12-05 21:07:26 -0800

    Releasing 1.0.0.

--- lein-swank/project.clj
+++ lein-swank/project.clj
@@ -1,4 +1,4 @@
-(defproject leiningen/lein-swank "1.0.0-SNAPSHOT"
+(defproject leiningen/lein-swank "1.0.0"
   :description "A leiningen plugin to launch a swank server."
   :dependencies [[swank-clojure "1.1.0-SNAPSHOT"]
                  [org.apache.maven/maven-ant-tasks "2.0.10"]])
