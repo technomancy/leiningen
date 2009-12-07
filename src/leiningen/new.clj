2c83f9dd72e26bdbadc464d8442bee18d3bf14f0
B.Smith-Mannschott <bsmith.occs@gmail.com>
2009-12-07 19:28:59 +0100

    teach "leiningen add" not to conflate project's name and directory.
    As a consequence it is now also possible to specify a groupId for a
    newly created project.
    
    lein new [group-id/]artifact-id [project-dir]
    
    - Group-id is optional. Project-dir defaults to artifact-id if not given.
    - Neither group-id nor artifact-id may contain slashes.
    - Underscore (_) is normalized to dash (-) in namespace names.
    - Dash (-) is normalized to underscore (_) in file and directory names.

--- src/leiningen/new.clj
+++ src/leiningen/new.clj
@@ -1,33 +1,48 @@
 (ns leiningen.new
-  "Create a new project skeleton."
+  "Create a new project skeleton.
+lein new [group-id/]artifact-id [project-dir]
+Group-id is optional. Project-dir defaults to artifact-id if not given.
+Neither group-id nor artifact-id may contain slashes."
   (:use [clojure.contrib.duck-streams :only [spit]]
         [clojure.contrib.java-utils :only [file]]
         [clojure.contrib.str-utils :only [str-join]]))
 
-(defn new [project-name & [args]]
-  (.mkdirs (file project-name))
-  (spit (file project-name "project.clj")
-        (pr-str (list 'defproject (symbol "leiningen") "1.0.0-SNAPSHOT"
-                      :description "FIXME: write"
-                      :dependencies [['org.clojure/clojure
-                                      "1.1.0-alpha-SNAPSHOT"]
-                                     ['org.clojure/clojure-contrib
-                                      "1.0-SNAPSHOT"]])))
-  (doseq [d [(str "src/" project-name) "test" "lib" "classes"]]
-    (.mkdirs (file project-name d)))
-  ;; maybe keep this somewhere else?
-  (spit (file project-name "src" (str project-name ".clj"))
-        (str "(ns " project-name ".core)\n"))
-  (spit (file project-name ".gitignore")
-        (str-join "\n" ["pom-generated.xml"
-                        "Manifest.txt"
-                        (str project-name ".jar")]))
-  (spit (file project-name "lib" ".gitignore") "*")
-  (spit (file project-name "classes" ".gitignore") "*")
-  (spit (file project-name "README")
-        (str-join "\n\n" [(str "# " project-name)
-                          "FIXME: write description"
-                          "## Usage" "FIXME: write"
-                          "## Installation" "FIXME: write"
-                          "## License" "FIXME: write\n"]))
-  (println "Created new project:" project-name))
+(defn str-replace [subs s]
+  (apply str (replace subs s)))
+
+(defn new [project-name & [_ project-dir]]
+  ; Clojure symbols conventionally use -, not _ to separate words in a
+  ; compound name.  When such compound names are used for as
+  ; directories or java packages, however, we must use _ in place of -.
+  (let [project-name (symbol (str-replace {\_ \-} project-name))
+        artifact-id (name project-name)
+        project-dir (or project-dir (str-replace {\- \_} artifact-id))]
+    (.mkdirs (file project-dir))
+    (spit (file project-dir "project.clj")
+          (pr-str (list 'defproject project-name "1.0.0-SNAPSHOT"
+                        :description "FIXME: write"
+                        :dependencies [['org.clojure/clojure
+                                        "1.1.0-alpha-SNAPSHOT"]
+                                       ['org.clojure/clojure-contrib
+                                        "1.0-SNAPSHOT"]])))
+    (let [starter-clj-ns   (str-replace {\/ \.} (str project-name))
+          starter-clj (str (str-replace {\- \_ \. \/} starter-clj-ns) ".clj")
+          starter-clj-dir  (.getParent (file starter-clj))]
+      (doseq [d [(str "src/" starter-clj-dir) "test" "lib" "classes"]]
+        (.mkdirs (file project-dir d)))
+      ;; maybe keep this somewhere else?
+      (spit (file project-dir "src" starter-clj)
+            (str "(ns " starter-clj-ns ")\n"))
+      (spit (file project-dir ".gitignore")
+            (str-join "\n" ["pom-generated.xml"
+                            "Manifest.txt"
+                            (str artifact-id ".jar")]))
+      (spit (file project-dir "lib" ".gitignore") "*")
+      (spit (file project-dir "classes" ".gitignore") "*")
+      (spit (file project-dir "README")
+            (str-join "\n\n" [(str "# " project-name)
+                              "FIXME: write description"
+                              "## Usage" "FIXME: write"
+                              "## Installation" "FIXME: write"
+                              "## License" "FIXME: write\n"]))
+      (println "Created new project in:" project-dir))))
