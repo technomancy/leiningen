a59bc8d5af31891635f7e8bc41fe1c452d7b3864
Phil Hagelberg <technomancy@gmail.com>
2009-12-07 21:22:02 -0800

    Call .replace instead of str-replace. Don't create lib or classes, just ignore.

--- src/leiningen/new.clj
+++ src/leiningen/new.clj
@@ -7,9 +7,6 @@ Neither group-id nor artifact-id may contain slashes."
         [clojure.contrib.java-utils :only [file]]
         [clojure.contrib.str-utils :only [str-join]]))
 
-(defn str-replace [subs s]
-  (apply str (replace subs s)))
-
 (defn new [project-name & [project-dir]]
   (println project-name project-dir)
   (let [project-name (symbol project-name)
@@ -25,22 +22,19 @@ Neither group-id nor artifact-id may contain slashes."
                                         "1.1.0-alpha-SNAPSHOT"]
                                        ['org.clojure/clojure-contrib
                                         "1.0-SNAPSHOT"]])))
-    (let [starter-clj-ns   (str-replace {\/ \.} (str project-name))
-          starter-clj (str (str-replace {\- \_ \. \/} starter-clj-ns) ".clj")
-          starter-clj-dir  (.getParent (file starter-clj))]
-      (doseq [d [(str "src/" starter-clj-dir) "test" "lib" "classes"]]
-        (.mkdirs (file project-dir d)))
-      ;; maybe keep this somewhere else?
-      (spit (file project-dir "src" starter-clj)
-            (str "(ns " starter-clj-ns ")\n"))
+    (let [project-ns  (.replace (str project-name) "/" ".")
+          project-clj (str (.replace (str project-name) "-" "_") ".clj")]
+      (.mkdirs (file project-dir "test"))
+      (.mkdirs (.getParentFile (file project-dir "src" project-clj)))
+      (spit (file project-dir "src" project-clj)
+            (str "(ns " project-ns ")\n"))
       (spit (file project-dir ".gitignore")
             (str-join "\n" ["pom-generated.xml"
                             "Manifest.txt"
-                            (str artifact-id ".jar")]))
-      (spit (file project-dir "lib" ".gitignore") "*")
-      (spit (file project-dir "classes" ".gitignore") "*")
+                            (str artifact-id ".jar")
+                            "lib" "classes"]))
       (spit (file project-dir "README")
-            (str-join "\n\n" [(str "# " project-name)
+            (str-join "\n\n" [(str "# " artifact-id)
                               "FIXME: write description"
                               "## Usage" "FIXME: write"
                               "## Installation" "FIXME: write"
