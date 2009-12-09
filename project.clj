b3e37c438cb7c77c169cddf6c8c0e9ea16517bf5
Phil Hagelberg <technomancy@gmail.com>
2009-12-08 18:01:08 -0800

    Replace rlwrap with jline for repl task.

--- project.clj
+++ project.clj
@@ -8,6 +8,7 @@
   :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
                  [ant/ant-launcher "1.6.2"]
+                 [jline "0.9.94"]
                  [org.apache.maven/maven-ant-tasks "2.0.10"]]
   :dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]]
   :main leiningen.core)
