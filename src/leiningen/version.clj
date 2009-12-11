5646badf5ebe33b4a62cddff85cdd4ac3e11965a
Phil Hagelberg <technomancy@gmail.com>
2009-12-10 22:42:50 -0800

    Be more verbose about the JVM in lein version.

--- src/leiningen/version.clj
+++ src/leiningen/version.clj
@@ -2,4 +2,6 @@
   "Print Leiningen's version to standard out.")
 
 (defn version []
-  (println "Leiningen" (System/getProperty "leiningen.version")))
+  (println "Leiningen" (System/getProperty "leiningen.version")
+           "on Java" (System/getProperty "java.version")
+           (System/getProperty "java.vm.name")))
