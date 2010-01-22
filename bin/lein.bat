74c9f1c8396c14cf3156652b9d049731c9c5b3a3
Rob Wolfe <rw@smsnet.pl>
2010-01-22 15:10:45 +0100

    adding error messages to lein.bat

--- bin/lein.bat
+++ bin/lein.bat
@@ -9,8 +9,31 @@ rem and it should be copied on %CLOJURE_JAR% path
 
 set CLOJURE_VERSION=1.1.0
 set LEIN_VERSION=1.1.0-SNAPSHOT
+
+rem uncomment this and set paths explicitly 
+rem set LEIN_JAR=C:\Documents and Settings\wojcirob\.m2\repository\leiningen\leiningen\%LEIN_VERSION%\leiningen-%LEIN_VERSION%-standalone.jar
+rem set CLOJURE_JAR=C:\Documents and Settings\wojcirob\.m2\repository\org\clojure\clojure\%CLOJURE_VERSION%\clojure-%CLOJURE_VERSION%.jar
+
+
+if "x%1" == "xself-install" goto NO_SELF_INSTALL
+
+rem it is possible to set LEIN_JAR and CLOJURE_JAR variables manually
+rem so we don't overwrite them
+if "x%LEIN_JAR%" == "x" goto SET_LEIN
+goto ARGS_HANDLING
+if "x%CLOJURE_JAR%" == "x" goto SET_CLOJURE
+goto ARGS_HANDLING
+
+:SET_LEIN
 set LEIN_JAR=%HOMEDRIVE%%HOMEPATH%\.m2\repository\leiningen\leiningen\%LEIN_VERSION%\leiningen-%LEIN_VERSION%-standalone.jar
+
+:SET_CLOJURE
 set CLOJURE_JAR=%HOMEDRIVE%%HOMEPATH%\.m2\repository\org\clojure\clojure\%CLOJURE_VERSION%\clojure-%CLOJURE_VERSION%.jar
+
+:ARGS_HANDLING
+if not exist "%LEIN_JAR%" goto NO_LEIN_JAR
+if not exist "%CLOJURE_JAR%" goto NO_CLOJURE_JAR
+
 
 rem ##################################################
 rem count number of command line arguments
@@ -31,22 +54,52 @@ for /R ./lib %%a in (*.jar) do (
 set CLASSPATH=!CLASSPATH!"
 
 set CLASSPATH=%CLASSPATH%;"%LEIN_JAR%"
-if "%DEBUG%" == "" goto :RUN
+if "x%DEBUG%" == "x" goto RUN
 echo CLASSPATH=%CLASSPATH%
 rem ##################################################
 
 :RUN
-if "%ARGCOUNT%" == "2" goto :RUN_ARG2
-if "%ARGCOUNT%" == "3" goto :RUN_ARG3
+if "x%1" == "xrepl" goto RUN_REPL
+if "%ARGCOUNT%" == "2" goto RUN_ARG2
+if "%ARGCOUNT%" == "3" goto RUN_ARG3
 java -Xbootclasspath/a:"%CLOJURE_JAR%" -client -cp %CLASSPATH% clojure.main -e "(use 'leiningen.core) (-main \"%1\")"
-goto :EOF
+goto EOF
 
 :RUN_ARG2
 java -Xbootclasspath/a:"%CLOJURE_JAR%" -client -cp %CLASSPATH% clojure.main -e "(use 'leiningen.core) (-main \"%1\" \"%2\")"
-goto :EOF
+goto EOF
 
 :RUN_ARG3
 java -Xbootclasspath/a:"%CLOJURE_JAR%" -client -cp %CLASSPATH% clojure.main -e "(use 'leiningen.core) (-main \"%1\" \"%2\" \"%3\")"
-goto :EOF
+goto EOF
+
+:RUN_REPL
+java -Xbootclasspath/a:"%CLOJURE_JAR%" -client -cp src;classes;%CLASSPATH% clojure.main %2 %3 %4
+goto EOF
+
+:NO_LEIN_JAR
+echo.
+echo "%LEIN_JAR%" can not be found.
+echo Please change LEIN_JAR environment variable
+echo or edit lein.bat to set appropriate LEIN_JAR path.
+echo. 
+goto EOF
+
+:NO_CLOJURE_JAR
+echo.
+echo "%CLOJURE_JAR%" can not be found.
+echo Please change CLOJURE_JAR environment variable
+echo or edit lein.bat to set appropriate CLOJURE_JAR path.
+echo. 
+goto EOF
+
+:NO_SELF_INSTALL
+echo.
+echo SELF_INSTALL functionality is not available on Windows
+echo Please download needed JARs manually:
+echo 1. leiningen-%LEIN_VERSION%-standalone.jar from http://repo.technomancy.us/
+echo 2. clojure.jar from http://build.clojure.org/
+echo. 
+goto EOF
 
 :EOF
