bf2f63c9421b445391eeeb1da593b720e0978056
Phil Hagelberg <technomancy@gmail.com>
2010-02-12 12:30:48 -0800

    Modify test task so it doesn't load clojure.test in Leiningen's classloader.

--- src/leiningen/test.clj
+++ src/leiningen/test.clj
@@ -1,11 +1,7 @@
 (ns leiningen.test
   "Run the project's tests."
   (:refer-clojure :exclude [test])
-  ;; When using -Xbootclasspath transitive requires break, so we need
-  ;; to require stuff that clojure.test depends on explicitly:
-  (:require [clojure walk template stacktrace])
-  (:use [clojure.test]
-        [clojure.contrib.java-utils :only [file]]
+  (:use [clojure.contrib.java-utils :only [file]]
         [clojure.contrib.find-namespaces :only [find-namespaces-in-dir]]
         [leiningen.compile :only [eval-in-project]]))
 
@@ -19,11 +15,12 @@ each namespace and print an overall summary."
              summary# (reduce (fn [summary# n#]
                                 (require n# :reload-all)
                                 (merge-with add-numbers#
-                                            summary# (run-tests n#)))
+                                            summary#
+                                            (clojure.test/run-tests n#)))
                               {} '~namespaces)]
-         (with-test-out
+         (clojure.test/with-test-out
            (println "\n\n--------------------\nTotal:")
-           (report summary#)))))
+           (clojure.test/report summary#)))))
 
 (defn test
   "Run the project's tests. Accept a list of namespaces for which to run all
