ca975f95d88588b384cabe3893214d9e2ea02453
Phil Hagelberg <technomancy@gmail.com>
2010-03-11 21:22:38 -0800

    Update lein-swank version and deps.

--- lein-swank/project.clj
+++ lein-swank/project.clj
@@ -1,4 +1,6 @@
-(defproject leiningen/lein-swank "1.1.0"
+(defproject leiningen/lein-swank "1.2.0-SNAPSHOT"
   :description "A leiningen plugin to launch a swank server."
-  :dependencies [[swank-clojure "1.1.0"]
+  :dependencies [[swank-clojure "1.2.0-break-SNAPSHOT"]
+                 [ant/ant "1.6.5"]
+                 [ant/ant-launcher "1.6.5"]
                  [org.apache.maven/maven-ant-tasks "2.0.10"]])
