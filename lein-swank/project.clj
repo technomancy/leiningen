84ca66b0232e63fcf5184a49c5b3bc7d6b19250f
Phil Hagelberg <technomancy@gmail.com>
2010-03-21 15:18:32 -0700

    Accept host argument from the command line for lein-swank.

--- lein-swank/project.clj
+++ lein-swank/project.clj
@@ -1,6 +1,7 @@
 (defproject leiningen/lein-swank "1.2.0-SNAPSHOT"
   :description "A leiningen plugin to launch a swank server."
-  :dependencies [[swank-clojure "1.2.0-break-SNAPSHOT"]
+  :dependencies [[swank-clojure "1.2.0-SNAPSHOT"]
                  [ant/ant "1.6.5"]
                  [ant/ant-launcher "1.6.5"]
-                 [org.apache.maven/maven-ant-tasks "2.0.10"]])
+                 [org.apache.maven/maven-ant-tasks "2.0.10"]]
+  :dev-dependencies [[org.clojure/clojure "1.1.0"]])
--- lein-swank/src/leiningen/swank.clj
+++ lein-swank/src/leiningen/swank.clj
@@ -2,17 +2,18 @@
   (:use [leiningen.compile :only [eval-in-project]]))
 
 (defn swank
-  "Launch swank server for Emacs to connect."
-  ([project port & opts]
+  "Launch swank server for Emacs to connect. Optionally takes PORT and HOST."
+  ([project port host & opts]
      (eval-in-project project
                       `(do (try (require '~'swank.swank)
                                 (@(ns-resolve '~'swank.swank
                                               '~'start-repl)
                                  (Integer. ~port)
-				 ~@(map read-string opts))
+                                 ~@(concat (map read-string opts) [:host host]))
                                 (catch Exception e#
                                   (println e#)
                                   (println "Make sure swank-clojure is added as"
                                            "a dev-dependency in your"
                                            "project.clj."))))))
+  ([project port] (swank project port "localhost"))
   ([project] (swank project 4005)))
