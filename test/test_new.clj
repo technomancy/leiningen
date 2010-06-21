607c282a0a55d95819e2e8dcea3c81c0a80b0c80
Phil Hagelberg <technomancy@gmail.com>
2010-06-20 20:18:08 -0700

    Check for Windows in the tests. Fixes #32.

--- test/test_new.clj
+++ test/test_new.clj
@@ -4,6 +4,7 @@
         [clojure.contrib.shell :only [with-sh-dir sh]]))
 
 (deftest test-new
-  (sh "lein" "new" "a.b/test-new-proj")
+  (sh (if (= "Windows" (System/getProperty "os.name"))
+        "lein.bat" "lein") "new" "a.b/test-new-proj")
   (is (.exists (file "test-new-proj" "src" "a" "b" "test_new_proj" "core.clj")))
   (delete-file-recursively (file "test-new-proj") false))
