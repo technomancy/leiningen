fe2927a9f2255713871c36f3334440f7bac6f74d
Phil Hagelberg <technomancy@gmail.com>
2010-08-19 20:54:28 -0700

    Upgrade Leiningen and "new" task to Clojure 1.2.0 stable.

--- project.clj
+++ project.clj
@@ -6,12 +6,11 @@
   :description "A build tool designed not to set your hair on fire."
   :url "http://github.com/technomancy/leiningen"
   :license {:name "Eclipse Public License"}
-  :dependencies [[org.clojure/clojure "1.2.0-RC3"]
-                 [org.clojure/clojure-contrib "1.2.0-RC3"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [ant/ant "1.6.5"]
                  [jline "0.9.94"]
                  [robert/hooke "1.0.2"]
                  [org.apache.maven/maven-ant-tasks "2.0.10"]]
   :dev-dependencies [[swank-clojure "1.2.1"]]
-  :disable-implicit-clean true
-  :main leiningen.core)
+  :disable-implicit-clean true)
