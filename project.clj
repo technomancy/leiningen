ce99a4b9d56678e31eec2d4e8f2d088c8a1b4361
Phil Hagelberg <technomancy@gmail.com>
2010-09-23 20:15:42 -0700

    Strip out cross-platform crap from bin/lein.

--- project.clj
+++ project.clj
@@ -9,7 +9,6 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.apache.ant/ant "1.7.1"]
-                 [jline "0.9.94"]
                  [robert/hooke "1.1.0"]
                  [org.apache.maven/maven-ant-tasks "2.0.10" :exclusions [ant]]]
   :disable-implicit-clean true
