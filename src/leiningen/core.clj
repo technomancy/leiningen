742738d9d23a33bf33f4cda76b75f0d9af8ad6c8
Phil Hagelberg <technomancy@gmail.com>
2010-11-10 14:05:42 -0800

    Fix unquote-project to preserve forms in test-selectors.

--- src/leiningen/core.clj
+++ src/leiningen/core.clj
@@ -11,8 +11,7 @@
   (walk (fn [item]
           (cond (and (seq? item) (= `unquote (first item))) (second item)
                 ;; needed if we want fn literals to be usable by eval-in-project
-                (and (seq? item) (= 'fn (first item))) (list 'quote item)
-                (symbol? item) (list 'quote item)
+                (or (list? item) (symbol? item)) (list 'quote item)
                 :else (unquote-project item)))
         identity
         args))
