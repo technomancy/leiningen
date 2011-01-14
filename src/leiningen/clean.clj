1c82f852fc377e6b70d5e9bdadfd1053dcf98504
Phil Hagelberg <technomancy@gmail.com>
2011-01-14 09:42:52 -0500

    Fix docstring for clean task.

--- src/leiningen/clean.clj
+++ src/leiningen/clean.clj
@@ -1,10 +1,11 @@
 (ns leiningen.clean
-  "Remove compiled artifacts and jars from project."
+  "Remove compiled class files from project."
   (:use [leiningen.jar :only [get-jar-filename get-default-uberjar-name]]
         [leiningen.util.file :only [delete-file-recursively]]))
 
 (defn clean
-  "Remove compiled artifacts and jars from project."
+  "Remove compiled class files from project. Jars are not deleted; run deps task
+to delete all jars and get fresh ones."
   [project]
   (println "Cleaning up.")
   (doseq [f [(get-jar-filename project)
