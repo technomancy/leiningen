bf08da574225d8d237e623af48fae3dc52b0e49a
Phil Hagelberg <technomancy@gmail.com>
2011-01-24 19:39:01 -0800

    New project skeletons don't need contrib.

--- src/leiningen/new.clj
+++ src/leiningen/new.clj
@@ -10,8 +10,7 @@
   (spit (file project-dir "project.clj")
         (str "(defproject " project-name " \"1.0.0-SNAPSHOT\"\n"
              "  :description \"FIXME: write\"\n"
-             "  :dependencies [[org.clojure/clojure \"1.2.0\"]\n"
-             "                 [org.clojure/clojure-contrib \"1.2.0\"]])\n")))
+             "  :dependencies [[org.clojure/clojure \"1.2.0\"]])\n")))
 
 (defn write-implementation [project-dir project-clj project-ns]
   (.mkdirs (.getParentFile (file project-dir "src" project-clj)))
