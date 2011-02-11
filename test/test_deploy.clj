e3bf2d0454af46773788024eed015d9171a65c14
Matjaz Gregoric <mtyaka@gmail.com>
2011-02-11 23:52:35 +0100

    Fix test_deploy.

--- test/test_deploy.clj
+++ test/test_deploy.clj
@@ -3,14 +3,14 @@
   (:use [clojure.test]
         [clojure.java.io :only [file]]
         [leiningen.core :only [read-project defproject]]
-        [leiningen.util.file]))
+        [leiningen.util.file :only [delete-file-recursively tmp-dir]]))
 
 (def test-project (read-project "test_projects/sample/project.clj"))
 
 (deftest test-deploy
-  (delete-file-recursively "/tmp/lein-repo" :silently)
+  (delete-file-recursively (format "%s/lein-repo" tmp-dir) :silently)
   (deploy test-project "snapshots")
-  (let [dir (file "/tmp/lein-repo/nomnomnom/nomnomnom/0.5.0-SNAPSHOT/")
+  (let [dir (file tmp-dir "lein-repo/nomnomnom/nomnomnom/0.5.0-SNAPSHOT/")
         files (.list dir)
         year (+ 1900 (.getYear (java.util.Date.)))]
     (is (seq files))
