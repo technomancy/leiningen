43f8cac4a70144343fb00367efba05676a85dee7
Phil Hagelberg <technomancy@gmail.com>
2011-05-12 14:27:20 -0400

    Make retest work with test selectors.

--- src/leiningen/retest.clj
+++ src/leiningen/retest.clj
@@ -5,9 +5,10 @@
 (defn retest
   "Run only the test namespaces which failed last time around.
 Requires loading leiningen.hooks.retest to :hooks in project.clj."
-  [project]
+  [project & selectors]
   ;; TODO: detect branch change; clear failure list
-  (if (.exists (java.io.File. ".lein-failures"))
-    (apply leiningen.test/test project (->> (slurp ".lein-failures")
-                                            read-string sort (map name)))
-    (leiningen.test/test project)))
+  (apply leiningen.test/test project
+         (concat (if (.exists (java.io.File. ".lein-failures"))
+                   (->> (slurp ".lein-failures")
+                        read-string sort (map name)))
+                 selectors)))
