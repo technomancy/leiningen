1dded4a643f86b2e218b60f4f0db788e6c32131c
Phil Hagelberg <technomancy@gmail.com>
2011-06-18 21:46:06 -0700

    Add a warning to classpath task regarding :local-repo-classpath.
    Dear Ant: http://cdn.someecards.com/someecards/usercards/1299543184590_8002436.png
    
    Hopefully the switch to aether will fix this; can't stand the thought
    of digging around in ant's source to work around the bug for another minute.

--- src/leiningen/classpath.clj
+++ src/leiningen/classpath.clj
@@ -1,7 +1,7 @@
 (ns leiningen.classpath
   "Print the classpath of the current project."
   (:use [leiningen.core :only [read-project]]
-        [leiningen.deps :only [do-deps find-jars]]
+        [leiningen.deps :only [find-jars]]
         [leiningen.util.paths :only [leiningen-home]]
         [clojure.java.io :only [file]]
         [clojure.string :only [join]])
@@ -61,6 +61,10 @@
 (defn classpath
   "Print the classpath of the current project.
 
-Suitable for java's -classpath option."
+Suitable for java's -classpath option.
+
+Warning: due to a bug in ant, calling this task with :local-repo-classpath set
+when the dependencies have not been fetched will result in spurious output before
+the classpath. In such cases, pipe to tail -n 1."
   [project]
   (println (get-classpath-string project)))
