20d3bded4f7e97a620be7af4b951abfa073efbef
Phil Hagelberg <technomancy@gmail.com>
2011-06-28 22:04:41 -0700

    Try using a proper debian/ dir instead of lein-deb.

--- project.clj
+++ project.clj
@@ -2,28 +2,14 @@
 ;; defproject form. It can have other code in it as well, including
 ;; loading other task definitions.
 
-(defproject leiningen "1.6.0-SNAPSHOT"
+(defproject leiningen "1.6.0.20110628"
   :description "A build tool designed not to set your hair on fire."
   :url "https://github.com/technomancy/leiningen"
-  :license {:name "Eclipse Public License"}
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :license {:name "Eclipse Public License 1.0"}
+  :dependencies [;; TODO: declare these as .deb dependencies
                  [org.clojure/clojure-contrib "1.2.0"]
                  [clucy "0.2.1"]
                  [lancet "1.0.1"]
-                 [robert/hooke "1.1.1"]
-                 [org.apache.maven/maven-ant-tasks "2.0.10" :exclusions [ant]]]
-  :dev-dependencies [[lein-deb "1.0.0-SNAPSHOT"]]
+                 [robert/hooke "1.1.1"]]
   :disable-implicit-clean true
-  :eval-in-leiningen true
-  :deb {:maintainer {:name "Travis Vachon"
-                     :email "travis.vachon@gmail.com"}
-        :depends "ant1.7, rlwrap, libjtidy-java"
-        :filesets [{:dir "."
-                    :includes "leiningen*.jar"
-                    :prefix "/usr/share/java/lein"}
-                   {:dir "lib"
-                    :includes "hooke*.jar,clojure*.jar,maven*.jar,plexus*.jar,wagon*.jar,classworlds.jar"
-                    :prefix "/usr/share/java/lein"}
-                   {:file "bin/lein"
-                    :fullpath "/usr/bin/lein"
-                    :filemode "755"}]})
+  :eval-in-leiningen true)
