84e62691e1a1c7a4fc3f2995297a5efb72b9c1c7
Phil Hagelberg <technomancy@gmail.com>
2011-07-01 22:24:29 -0700

    First actually-working from-real-deb lein build.
    Modulo having to manually install contrib 1.2, clucy, hooke, and lancet.

--- project.clj
+++ project.clj
@@ -2,14 +2,9 @@
 ;; defproject form. It can have other code in it as well, including
 ;; loading other task definitions.
 
-(defproject leiningen "1.6.1-SNAPSHOT"
+(defproject leiningen "1.6.1.20110701"
   :description "A build tool designed not to set your hair on fire."
   :url "https://github.com/technomancy/leiningen"
   :license {:name "Eclipse Public License 1.0"}
-  :dependencies [;; TODO: declare these as .deb dependencies
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [clucy "0.2.1"]
-                 [lancet "1.0.1"]
-                 [robert/hooke "1.1.2"]]
   :disable-implicit-clean true
   :eval-in-leiningen true)
