bd466d751a156be1952dbea7abf510ba1e55b72e
Phil Hagelberg <technomancy@gmail.com>
2011-07-06 09:05:09 -0700

    Release 1.6.1.

--- project.clj
+++ project.clj
@@ -2,13 +2,13 @@
 ;; defproject form. It can have other code in it as well, including
 ;; loading other task definitions.
 
-(defproject leiningen "1.6.1-SNAPSHOT"
+(defproject leiningen "1.6.1"
   :description "Automate Clojure projects without setting your hair on fire."
   :url "https://github.com/technomancy/leiningen"
   :license {:name "Eclipse Public License"}
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [clucy "0.2.1"]
+                 [clucy "0.2.2"]
                  [lancet "1.0.1"]
                  [jline "0.9.94" :exclusions [junit]]
                  [robert/hooke "1.1.2"]
