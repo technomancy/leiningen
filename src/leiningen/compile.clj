69bdaa3a7186b07ba8753a6d6c098cdafa9f28cd
Phil Hagelberg <technomancy@gmail.com>
2011-07-21 20:04:09 -0700

    Separate JVM_OPTS and LEIN_JVM_OPTS. Fixes #233.

--- src/leiningen/compile.clj
+++ src/leiningen/compile.clj
@@ -79,27 +79,10 @@
           "NUL"
           "/dev/null")))
 
-;; Split this function out for better testability.
-(defn- get-raw-input-args []
-  (.getInputArguments (ManagementFactory/getRuntimeMXBean)))
-
-(defn ^{:internal true} get-input-args
-  "Returns a vector of input arguments, accounting for a bug in RuntimeMXBean
-  that splits arguments which contain spaces. Removes -Xbootclasspath as it
-  requires special handling on the called code."
-  []
-  ;; RuntimeMXBean.getInputArguments() is buggy when an input argument
-  ;; contains spaces. For an input argument of -Dprop="hello world" it
-  ;; returns ["-Dprop=hello", "world"]. Try to work around this bug.
-  (letfn [(join-broken-args [v arg] (if (= \- (first arg))
-                                      (conj v arg)
-                                      (conj (vec (butlast v))
-                                            (format "%s %s" (last v) arg))))]
-    (remove #(re-find #"^-Xbootclasspath.+" %)
-            (reduce join-broken-args [] (get-raw-input-args)))))
-
 (defn- get-jvm-args [project]
-  (concat (get-input-args) (:jvm-opts project) (:jvm-opts (user-settings))))
+  `(~@(when-let [opts (System/getenv "JVM_OPTS")] [opts])
+    ~@(:jvm-opts project)
+    ~@(:jvm-opts (user-settings))))
 
 (defn- injected-forms []
   (with-open [rdr (-> "robert/hooke.clj" resource reader PushbackReader.)]
