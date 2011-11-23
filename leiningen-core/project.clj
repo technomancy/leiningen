4eb88c786129baae33c95e9d7928d4e3a0850c52
Phil Hagelberg <technomancy@gmail.com>
2011-11-23 14:07:10 -0800

    Add explicit hooke dep to leiningen-core.

--- leiningen-core/project.clj
+++ leiningen-core/project.clj
@@ -4,4 +4,5 @@
   :description "Core functionality of Leiningen as a library."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [classlojure "0.6.3"]
+                 [robert/hooke "1.1.2"]
                  [com.cemerick/pomegranate "0.0.1"]])
