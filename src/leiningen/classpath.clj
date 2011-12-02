708a586242860614bc0eeccf6a31aa80a6cbed4f
Phil Hagelberg <technomancy@gmail.com>
2011-12-02 15:52:43 -0800

    Use leiningen.core.classpath from classpath task.

--- src/leiningen/classpath.clj
+++ src/leiningen/classpath.clj
@@ -1,56 +1,10 @@
 (ns leiningen.classpath
   "Print the classpath of the current project."
-  (:use [leiningen.core :only [read-project no-dev?]]
-        [leiningen.deps :only [find-deps-files]]
-        [leiningen.util.paths :only [leiningen-home]]
-        [clojure.java.io :only [file]]
-        [clojure.string :only [join]]))
-
-(defn- read-dependency-project [dep]
-  (let [project (.getAbsolutePath (file dep "project.clj"))]
-    (try (read-project project)
-         (catch Exception e
-           (throw (Exception. (format "Problem loading %s" project) e))))))
-
-(defn- ensure-absolute [path root]
-  (.getCanonicalPath
-   (let [f (file path)]
-     (if (.isAbsolute f)
-       f
-       (file root f)))))
-
-(defn checkout-deps-paths [project]
-  (apply concat (for [dep (.listFiles (file (:root project) "checkouts"))
-                      ;; Note that this resets the leiningen.core/project var!
-                      :let [proj (read-dependency-project dep)]
-                      :when proj]
-                  (for [d (:checkout-deps-shares project [:source-path
-                                                          :compile-path
-                                                          :resources-path])]
-                    (ensure-absolute (d proj) dep)))))
-
-(defn user-plugins []
-  (for [jar (.listFiles (file (leiningen-home) "plugins"))
-        :when (re-find #"\.jar$" (.getName jar))]
-    (.getAbsolutePath jar)))
-
-(defn get-classpath
-  "Answer a list of classpath entries for PROJECT."
-  [project]
-  (concat (if-not (no-dev?)
-            [(:test-path project)
-             (:dev-resources-path project)])
-          [(:source-path project)
-           (:compile-path project)
-           (:resources-path project)]
-          (:extra-classpath-dirs project)
-          (checkout-deps-paths project)
-          (find-deps-files project)
-          (if-not (no-dev?)
-            (user-plugins))))
+  (:require [leiningen.core.classpath :as classpath]
+            [clojure.string :as str]))
 
 (defn get-classpath-string [project]
-  (join java.io.File/pathSeparatorChar (get-classpath project)))
+  (str/join java.io.File/pathSeparatorChar (classpath/get-classpath project)))
 
 (defn classpath
   "Print the classpath of the current project.
