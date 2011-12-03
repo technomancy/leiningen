cd5cd2c0d96249cdf79dc37d44938973779e69b8
Nelson Morris <nmorris@nelsonmorris.net>
2011-12-02 21:25:57 -0600

    Update the run task tests for 2.0
    The test.helper needed to change the project map for the test projects
    because eval-in-project uses get-classpath, which will return the
    relative paths for some directories.  This breaks in this case since
    the working directory is not the root of the test project.

--- test_projects/tricky-name/project.clj
+++ test_projects/tricky-name/project.clj
@@ -1,6 +1,6 @@
 (defproject org.domain/tricky-name "1.0"
   :description "One with a tricky group and project name"
-  :dev-dependencies [[clojure "1.2.0"]]
+  :dev-dependencies [[org.clojure/clojure "1.2.0"]]
   :shell-wrapper true
   :main ^{:skip-aot true} org.domain.tricky-name.core
   :run-aliases {:bbb org.domain.tricky-name.brunch
