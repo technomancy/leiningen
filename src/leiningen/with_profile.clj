cfb0714d8e8cc0530b73445ec47dba19aae05b1e
Phil Hagelberg <technomancy@gmail.com>
2011-12-09 21:17:50 -0800

    Add with-profile task.

---
+++ src/leiningen/with_profile.clj
@@ -0,0 +1,11 @@
+(ns leiningen.with-profile
+  (:require [leiningen.main :as main]
+            [leiningen.core.project :as project]))
+
+(defn with-profile
+  "Apply the given task with the profile(s) specified. Multiple
+  comma-separated profiles may be given."
+  [project profiles task-name & args]
+  (let [profiles (map keyword (.split profiles ","))
+        project (project/merge-profiles project profiles)]
+    (main/apply-task task-name project args)))
