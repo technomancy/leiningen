135f3ee4d07c7f77416fef1fa5d137c00c8a624f
Phil Hagelberg <technomancy@gmail.com>
2011-12-20 21:29:26 -0800

    Bump to 1.6.3-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
 ;; defproject form. It can have other code in it as well, including
 ;; loading other task definitions.
 
-(defproject leiningen "1.6.2"
+(defproject leiningen "1.6.3-SNAPSHOT"
   :description "Automate Clojure projects without setting your hair on fire."
   :url "https://github.com/technomancy/leiningen"
   :license {:name "Eclipse Public License"}
