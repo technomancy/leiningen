45500dc0766bd0072aec55311dfff3b5c352506a
Phil Hagelberg <technomancy@gmail.com>
2012-01-11 17:21:53 -0800

    Fix default skeleton. org.clojure/clojure is no longer special-cased.

--- src/leiningen/new/default/project.clj
+++ src/leiningen/new/default/project.clj
@@ -1,4 +1,4 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   ;; :url "http://FIXME"
-  :dependencies [[clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]])
