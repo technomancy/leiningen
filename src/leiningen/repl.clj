a7520fd50d3705502aea8d2456d6ac18134b2978
Anthony Grimes <i@raynes.me>
2012-02-06 20:27:58 -0600

    Add connection polling.

--- src/leiningen/repl.clj
+++ src/leiningen/repl.clj
@@ -11,15 +11,29 @@
                         `(.get (nth (clojure.tools.nrepl/start-server ~port) 1))
                         '(require 'clojure.tools.nrepl)))
 
-(defn get-port []
+(defn- get-port []
   (Integer.
    (or (System/getenv "LEIN_REPL_PORT")
        (dec (+ 1024 (rand-int 64512))))))
+
+(def retry-limit 200)
+
+(defn- poll-repl-connection
+  ([port retries]
+     (when (> retries retry-limit)
+       (throw (Exception. "Couldn't connect")))
+     (Thread/sleep 100)
+     (let [val (try (reply/launch-nrepl {:attach (str port)})
+                    (catch java.net.ConnectException _ ::retry))]
+       (if (= ::retry val)
+         (recur port (inc retries))
+         val)))
+  ([port]
+     (poll-repl-connection port 0)))
 
 (defn repl
   ([] (repl nil))
   ([project]
      (let [port (get-port)]
        (future (start-server project port))
-       (Thread/sleep 4000)
-       (reply/launch-nrepl {:attach (str port)}))))
+       (poll-repl-connection port))))
