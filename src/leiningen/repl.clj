c68b07bae0896ffbf8310791fa0fb7008a723a68
Anthony Grimes <i@raynes.me>
2012-02-06 22:07:27 -0600

    Allow the task to work outside of projects.

--- src/leiningen/repl.clj
+++ src/leiningen/repl.clj
@@ -8,13 +8,15 @@
             [leiningen.core.classpath :as classpath]))
 
 (defn- start-server [project ack-port]
-  (eval/eval-in-project project
-                        `(clojure.tools.nrepl/start-server 0 ~ack-port)
-                        '(require 'clojure.tools.nrepl)))
+  (if project
+    (eval/eval-in-project project
+                          `(clojure.tools.nrepl/start-server 0 ~ack-port)
+                          '(require 'clojure.tools.nrepl))
+    (nrepl/start-server 0 ack-port)))
 
 (def lein-repl-server (delay (nrepl/start-server)))
 
-(defn repl
+(defn ^:no-project-needed repl
   ([] (repl nil))
   ([project]
    (nrepl/reset-ack-port!)
