5844f0450d5a4bc4e349e686d2e78530719ec4c8
Anthony Grimes <i@raynes.me>
2012-02-06 21:33:59 -0600

    Use nrepl's ack mechanism instead of manual polling.

--- src/leiningen/repl.clj
+++ src/leiningen/repl.clj
@@ -4,36 +4,19 @@
             [reply.main :as reply]
             [clojure.java.io :as io]
             [leiningen.core.eval :as eval]
+            [clojure.tools.nrepl :as nrepl]
             [leiningen.core.classpath :as classpath]))
 
-(defn- start-server [project port]
+(defn- start-server [project ack-port]
   (eval/eval-in-project project
-                        `(.get (nth (clojure.tools.nrepl/start-server ~port) 1))
+                        `(clojure.tools.nrepl/start-server 0 ~ack-port)
                         '(require 'clojure.tools.nrepl)))
 
-(defn- get-port []
-  (Integer.
-   (or (System/getenv "LEIN_REPL_PORT")
-       (dec (+ 1024 (rand-int 64512))))))
-
-(def retry-limit 200)
-
-(defn- poll-repl-connection
-  ([port retries]
-     (when (> retries retry-limit)
-       (throw (Exception. "Couldn't connect")))
-     (Thread/sleep 100)
-     (let [val (try (reply/launch-nrepl {:attach (str port)})
-                    (catch java.net.ConnectException _ ::retry))]
-       (if (= ::retry val)
-         (recur port (inc retries))
-         val)))
-  ([port]
-     (poll-repl-connection port 0)))
+(defonce lein-repl-server (nrepl/start-server))
 
 (defn repl
   ([] (repl nil))
   ([project]
-     (let [port (get-port)]
-       (future (start-server project port))
-       (poll-repl-connection port))))
+   (nrepl/reset-ack-port!)
+   (future (start-server project (-> lein-repl-server first .getLocalPort))) 
+   (reply/launch-nrepl {:attach (str (nrepl/wait-for-ack 20000))})))
