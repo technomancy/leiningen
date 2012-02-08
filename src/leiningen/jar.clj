78560e8d4a5f92c5a6483fa7365902f3de6f26e8
Phil Hagelberg <technomancy@gmail.com>
2012-02-07 20:40:36 -0800

    Use :without-profiles version of project map in jar task.

--- src/leiningen/jar.clj
+++ src/leiningen/jar.clj
@@ -179,6 +179,7 @@ as .class files if applicable. If project.clj contains a :main key, the -main
 function in that namespace will be used as the main-class for executable jar."
   [project]
   (let [classpath [] ;; (classpath/resolve-dependencies project)
+        project (:without-profiles (meta project))
         status (compile/compile project)]
     (if (zero? status)
       (let [jar-file (get-jar-filename project)]
