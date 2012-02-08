c75ba9ec00650be9b1286e37be6030f287594830
Phil Hagelberg <technomancy@gmail.com>
2012-02-08 14:45:38 -0800

    Allow profiles arg to pprint a given profile.

--- src/leiningen/profiles.clj
+++ src/leiningen/profiles.clj
@@ -1,16 +1,25 @@
 (ns leiningen.profiles
   (:require [clojure.string]
+            [clojure.pprint :as pprint]
             [leiningen.core.project :as project]
             [leiningen.core.user :as user]))
 
+(defn- all-profiles [project]
+  (merge (deref project/default-profiles)
+         (user/profiles)
+         (:profiles project)))
+
 (defn ^:no-project-needed profiles
-  "List all available profiles."
-  [project]
-  (->> (merge (deref project/default-profiles)
-              (user/profiles)
-              (:profiles project))
-       keys
-       (map name)
-       sort
-       (clojure.string/join "\n")
-       println))
+  "List all available profiles or display one if given an argument."
+  ([project]
+     (->> (all-profiles project)
+          (keys)
+          (map name)
+          (sort)
+          (clojure.string/join "\n")
+          (println)))
+  ([project profile]
+     (-> (all-profiles project)
+         (get (keyword profile))
+         (pprint/pprint))
+     (flush)))
