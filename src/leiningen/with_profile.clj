81e405755f1bd09867c2c6f65ec61ee8b690e53c
Lee Hinman <lee@writequit.org>
2012-02-09 15:27:17 -0700

    Allow colon-separated profiles to sequential task application
    Allows something like this:
    
    % lein2 with-profile dev,1.2:1.4,user test
    Performing task 'test' with profile(s): 'dev,1.2'
    
    Testing clj-http.test.client
    
    Testing clj-http.test.cookies
    
    Testing clj-http.test.core
    
    Ran 47 tests containing 174 assertions.
    0 failures, 0 errors.
    Performing task 'test' with profile(s): '1.4,user'
    
    Testing clj-http.test.client
    
    Testing clj-http.test.cookies
    
    Testing clj-http.test.core
    
    Ran 47 tests containing 174 assertions.
    0 failures, 0 errors.
    
    Also adds the println for performing tasks with 'with-profile', not
    sure if this should stay or not.

--- src/leiningen/with_profile.clj
+++ src/leiningen/with_profile.clj
@@ -2,11 +2,21 @@
   (:require [leiningen.core.main :as main]
             [leiningen.core.project :as project]))
 
-(defn with-profile
-  "Apply the given task with the profile(s) specified.
-
-Multiple comma-separated profiles may be given."
+(defn- with-profile*
+  "Apply the given task with a comma-separeted profile list."
   [project profiles task-name & args]
   (let [profiles (map keyword (.split profiles ","))
         project (project/merge-profiles project profiles)]
     (main/apply-task task-name project args)))
+
+(defn with-profile
+  "Apply the given task with the profile(s) specified.
+
+Comma-separated profiles may be given to merged profiles and perform the task.
+Colon-separated profiles may be given for sequential profile application."
+  [project profiles task-name & args]
+  (let [profile-groups (seq (.split profiles ":"))]
+    (doseq [profile-group profile-groups]
+      (println (format "Performing task '%s' with profile(s): '%s'"
+                       task-name profile-group))
+      (apply with-profile* project profile-group task-name args))))
