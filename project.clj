20b6c0415aea601501d1c88bce09e6f84a98fd17
Phil Hagelberg <technomancy@gmail.com>
2012-02-16 13:12:48 -0800

    Bump clucy version to avoid dynamicity warnings.

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[leiningen-core "2.0.0-SNAPSHOT"]
-                 [clucy "0.2.2"]
+                 [clucy "0.2.3"]
                  [useful "0.7.6-alpha1"]
                  [lein-newnew "0.2.2"]
                  [reply "0.1.0-alpha1"]
