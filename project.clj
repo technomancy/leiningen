1d84877814d32664f5f4a4664c56f3c399289384
Colin Jones <trptcolin@gmail.com>
2012-02-19 09:21:25 -0600

    Bump reply for more CLJ-855 fun

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [clucy "0.2.2"]
                  [useful "0.7.6-alpha1"]
                  [lein-newnew "0.2.2"]
-                 [reply "0.1.0-alpha3"]
+                 [reply "0.1.0-alpha4"]
                  [org.clojars.ninjudd/data.xml "0.0.1-SNAPSHOT"]]
   :test-selectors {:default (complement :busted)}
   :eval-in-leiningen true)
