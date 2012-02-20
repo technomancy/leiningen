36791e3997ecdea1c291204190b699a90a6d321a
Phil Hagelberg <technomancy@gmail.com>
2012-02-19 21:18:29 -0800

    Check for :repository-auth when deploying. Fixes #402.

--- src/leiningen/deploy.clj
+++ src/leiningen/deploy.clj
@@ -1,6 +1,7 @@
 (ns leiningen.deploy
   "Build and deploy jar to remote repository."
-  (:require [cemerick.pomegranate.aether :as aether])
+  (:require [cemerick.pomegranate.aether :as aether]
+            [leiningen.core.classpath :as classpath])
   (:use [leiningen.jar :only [jar]]
         [leiningen.pom :only [pom snapshot?]]
         [clojure.java.io :only [file]]))
@@ -13,27 +14,26 @@ versions will go to the repo named \"snapshots\" while stable versions
 will go to \"releases\". You can also deploy to another repository
 in :repositories by providing its name as an argument.
 
-  :repositories {\"java.net\" \"http://download.java.net/maven/2\"
-                 \"snapshots\" {:url \"https://blueant.com/archiva/snapshots\"
-                                :username \"milgrim\" :password \"locative\"}
-                 \"releases\" {:url \"https://blueant.com/archiva/internal\"
-                               :private-key \"etc/id_dsa\"}}
+  :repositories {\"snapshots\" \"https://internal.repo/snapshots\"
+                 \"releases\" \"https://internal.repo/releases\"
+                 \"alternate\" \"https://other.server/repo\"}
 
-You can set authentication options keyed by repository name in
-~/.lein/init.clj to avoid checking sensitive information into source
-control:
+You should set authentication options keyed by repository URL in
+the :deploy profile in ~/.lein/profiles.clj to avoid checking
+sensitive information into source control:
 
-  (def leiningen-auth {\"https://blueant.com/archiva/internal\"
-                       {:passphrase \"vorpalbunny\"}})
+  {:user {:plugins [...]}
+   :auth {:repository-auth {\"https://internal.repo/snapshots\"
+                            {:username \"milgrim\" :password \"locative}}}}
 "
   ([project repository-name]
      (let [jarfile (jar project)
            pomfile (pom project)
-           repo-opts (or (get (apply hash-map (flatten (:deploy-repositories project))) repository-name)
-                         (get (apply hash-map (flatten (:repositories project))) repository-name))
-           repo (if repo-opts
-                  {repository-name repo-opts}
-                  {"inline" repository-name})]
+           repo-opts (or ((into {} (:deploy-repositories project)) repository-name)
+                         ((into {} (:repositories project)) repository-name))
+           repo (repo/add-repo-auth (if repo-opts
+                                      {repository-name repo-opts}
+                                      {"inline" {:url repository-name}}))]
        (if (number? jarfile)
          ;; if we failed to create the jar, return the status code for exit
          jarfile
