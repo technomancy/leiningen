0a649ff10422588ecbdcb14ae6c3bf8286e7c2a2
Chas Emerick <chas@cemerick.com>
2012-02-22 10:13:41 -0500

    re-enable accepting repository URI as argument to `deploy`

--- src/leiningen/deploy.clj
+++ src/leiningen/deploy.clj
@@ -12,7 +12,8 @@
 The target repository will be looked up in :repositories: snapshot
 versions will go to the repo named \"snapshots\" while stable versions
 will go to \"releases\". You can also deploy to another repository
-in :repositories by providing its name as an argument.
+in :repositories by providing its name as an argument, or specify
+the repository URL directly.
 
   :repositories {\"snapshots\" \"https://internal.repo/snapshots\"
                  \"releases\" \"https://internal.repo/releases\"
@@ -30,9 +31,10 @@ sensitive information into source control:
            pomfile (pom project)
            repo-opts (or (get (:deploy-repositories project) repository-name)
                          (get (:repositories project) repository-name))
-           repo (classpath/add-repo-auth (if (map? repo-opts)
-                                           [repository-name repo-opts]
-                                           [repository-name {:url repo-opts}]))]
+           repo (classpath/add-repo-auth (cond
+                                           (not repo-opts) ["inline" {:url repository-name}]
+                                           (map? repo-opts) [repository-name repo-opts]
+                                           :else [repository-name {:url repo-opts}]))]
        (if (number? jarfile)
          ;; if we failed to create the jar, return the status code for exit
          jarfile
