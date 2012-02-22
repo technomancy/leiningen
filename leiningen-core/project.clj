0a2e84754e0093e13f9f41cdb9147ac4b1465d39
Chas Emerick <chas@cemerick.com>
2012-02-22 08:46:16 -0500

    upgrade to pomegranate 0.0.7

--- leiningen-core/project.clj
+++ leiningen-core/project.clj
@@ -7,5 +7,5 @@
                  [classlojure "0.6.5"]
                  [robert/hooke "1.1.2"]
                  [ordered "1.1.0"]
-                 [com.cemerick/pomegranate "0.0.6"
+                 [com.cemerick/pomegranate "0.0.7"
                   :exclusions [org.slf4j/slf4j-api]]])
