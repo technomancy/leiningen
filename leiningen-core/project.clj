5c66dc0d2274c93553b5fb473baf96bc26e00acc
Chas Emerick <chas@cemerick.com>
2012-02-23 11:59:52 -0500

    Upgrade pomegranate to 0.0.8, fixes gh-422

--- leiningen-core/project.clj
+++ leiningen-core/project.clj
@@ -7,5 +7,5 @@
                  [classlojure "0.6.5"]
                  [robert/hooke "1.1.2"]
                  [ordered "1.1.0"]
-                 [com.cemerick/pomegranate "0.0.7"
+                 [com.cemerick/pomegranate "0.0.8"
                   :exclusions [org.slf4j/slf4j-api]]])
