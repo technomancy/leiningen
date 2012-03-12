c7b92d1b7849d70256465731f0fd41b6e14a53fb
Michael S. Klishin <michael@novemberain.com>
2012-03-12 08:11:40 +0400

    One more test case for the lein new task, includes artifact group in project name

--- test/leiningen/test/new.clj
+++ test/leiningen/test/new.clj
@@ -4,9 +4,16 @@
         [clojure.java.io :only [file]]
         [leiningen.test.helper :only [delete-file-recursively]]))
 
-(deftest test-new
+(deftest test-new-with-just-project-name
   (leiningen.new/new nil "test-new-proj")
   (is (= #{"README.md" "project.clj" "src" "core.clj" "test"
            "test_new_proj" "core_test.clj" ".gitignore"}
          (set (map (memfn getName) (rest (file-seq (file "test-new-proj")))))))
   (delete-file-recursively (file "test-new-proj") false))
+
+(deftest test-new-with-group-and-project-name
+  (leiningen.new/new nil "orgname/a-project")
+  (is (= #{"README.md" "project.clj" "orgname" "src" "core.clj" "test"
+           "a_project" "core_test.clj" ".gitignore"}
+         (set (map (memfn getName) (rest (file-seq (file "a-project")))))))
+  (delete-file-recursively (file "a-project") false))
