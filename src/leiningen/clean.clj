aa2888c4fd4a5989136369966ef81f2a5a12700a
Phil Hagelberg <technomancy@gmail.com>
2012-03-16 10:27:44 -0700

    Fix clean GC slowdown bug.

--- src/leiningen/clean.clj
+++ src/leiningen/clean.clj
@@ -1,17 +1,21 @@
 (ns leiningen.clean
   "Remove all files from project's target-path."
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [leiningen.core.eval :as eval]))
 
 (defn delete-file-recursively
   "Delete file f. If it's a directory, recursively delete all its contents.
 Raise an exception if any deletion fails unless silently is true."
   [f & [silently]]
-  (System/gc) ; This sometimes helps release files for deletion on windows.
   (let [f (io/file f)]
     (when (.isDirectory f)
       (doseq [child (.listFiles f)]
         (delete-file-recursively child silently)))
-    (io/delete-file f silently)))
+    (io/delete-file f silently)
+    ;; This sometimes helps release files for deletion on windows, but
+    ;; is slow as the dickens.
+    (when (= :windows (eval/get-os))
+      (System/gc))))
 
 (defn clean
   "Remove all files from project's target-path."
