4d0dda90043edaef052faa4db5bb4f5abc2309b1
Lee Hinman <lee@writequit.org>
2012-04-08 16:15:38 -0600

    bump clj-http dependency

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                  [reply "0.1.0-beta1"]
                  [org.clojure/data.xml "0.0.3"]
                  [bultitude "0.1.4"]
-                 [clj-http "0.3.3"]]
+                 [clj-http "0.3.6"]]
   ;; checkout-deps don't work with :eval-in :leiningen
   :profiles {:dev {:resource-paths ["leiningen-core/dev-resources"]
                    :test-paths ["leiningen-core/test"]}
