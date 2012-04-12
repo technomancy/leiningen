030632acf6bc797a9d723f5aface63375c781f39
Phil Hagelberg <technomancy@gmail.com>
2012-04-12 10:37:34 -0700

    Bump back to snapshot.

--- leiningen-core/project.clj
+++ leiningen-core/project.clj
@@ -1,4 +1,4 @@
-(defproject leiningen-core "2.0.0-preview3"
+(defproject leiningen-core "2.0.0-SNAPSHOT"
   :url "https://github.com/technomancy/leiningen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
