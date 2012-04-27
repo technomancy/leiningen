fcf63f39c599488fb8219800bf962ff924b51b66
Michael S. Klishin <michael@novemberain.com>
2012-04-28 03:14:46 +0400

    Translate most common javac options in Leiningen 1 format into the one used by Leiningen 2
    We intentionally don't try to support every single Lein 1 option because there is no way
    to do everything Ant javac task can do with JDK's javac API. And it's also not necessary
    because many of those features are very opinionated at best.

--- test/leiningen/test/javac.clj
+++ test/leiningen/test/javac.clj
@@ -1,9 +1,25 @@
 (ns leiningen.test.javac
   (:use [clojure.test]
         [clojure.java.io :only [file]]
-        [leiningen.javac :only [javac]]
+        [leiningen.javac :only [javac normalize-javac-options]]
         [leiningen.test.helper :only [delete-file-recursively
                                       #_dev-deps-project]]))
+
+(deftest test-javac-options-normalization
+  (testing "that Leiningen 2 style options are returned unmodified"
+    (are [arg] (is (= arg (normalize-javac-options arg)))
+      ["-target" "1.6" "-source" "1.6"]
+      ["-deprecation" "-g"]))
+  (testing "conversion of Leiningen 1 style options that are supported"
+    (are [old new] (is (= new (normalize-javac-options old)))
+         {:debug false}                ["-g:none"]
+         {:debug "off"}                ["-g:none"]
+         ;; overriden by :compile-path
+         {:destdir "clazzez"}          []
+         {:encoding "utf8"}            ["-encoding" "utf8"]
+         {:source "1.5" :target "1.5"} ["-target" "1.5" "-source" "1.5"]
+         {:source 1.5   "target" 1.5} ["-target" "1.5" "-source" "1.5"]
+         {:debugLevel "source,lines"}  ["-g:source,lines"])))
 
 (deftest ^:post-preview ; not really; need to fix this
   test-javac
