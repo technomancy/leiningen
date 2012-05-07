5cbe4435606480d777a547e026be81244506344f
Phil Hagelberg <technomancy@gmail.com>
2012-05-07 11:49:18 -0700

    Allow URL scheme to be omitted in $http_proxy.
    Seems like most tools allow this to be omitted, so we should probably
    follow suit.

--- leiningen-core/src/leiningen/core/classpath.clj
+++ leiningen-core/src/leiningen/core/classpath.clj
@@ -80,7 +80,9 @@
   "Returns a map of the JVM proxy settings"
   []
   (when-let [proxy (System/getenv "http_proxy")]
-    (let [url (URL. proxy)
+    (let [url (try (URL. proxy)
+                   (catch java.net.MalformedURLException _
+                     (URL. (str "http://" proxy))))
           user-info (.getUserInfo url)
           [username password] (and user-info (.split user-info ":"))]
       {:host (.getHost url)
