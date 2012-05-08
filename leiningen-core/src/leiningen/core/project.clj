262c669b26867627854427454503d7ca1e4354e0
David Santiago <david.santiago@gmail.com>
2012-05-07 17:10:44 -0700

    Make merge-profile private.

--- leiningen-core/src/leiningen/core/project.clj
+++ leiningen-core/src/leiningen/core/project.clj
@@ -148,7 +148,7 @@
 
         :else (doto latter (println "has a type mismatch merging profiles."))))
 
-(defn merge-profile [project profile]
+(defn- merge-profile [project profile]
   (merge-with profile-key-merge project profile))
 
 (defn- lookup-profile [profiles profile-name]
