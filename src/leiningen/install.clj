9920bd237f884da72b5ba06a8eb02b9f7a4f920b
Phil Hagelberg <technomancy@gmail.com>
2012-05-07 17:32:08 -0700

    Tidy up install task.

--- src/leiningen/install.clj
+++ src/leiningen/install.clj
@@ -8,40 +8,13 @@
   (:import (java.util.jar JarFile)
            (java.util UUID)))
 
-;; (defn bin-path []
-;;   (doto (file (leiningen-home) "bin") .mkdirs))
-
-;; (defn install-shell-wrappers [jarfile]
-;;   (when-let [bin-name ((manifest-map (.getManifest jarfile))
-;;                        "Leiningen-shell-wrapper")]
-;;     (let [entry-paths (if (= :windows (get-os))
-;;                         [bin-name (format "%s.bat" bin-name)]
-;;                         [bin-name])]
-;;       (doseq [entry-path entry-paths]
-;;         (let [bin-file (file (bin-path) (last (.split entry-path "/")))]
-;;           (when-let [zip-entry (.getEntry jarfile entry-path)]
-;;             (.mkdirs (.getParentFile bin-file))
-;;             (println "Installing shell wrapper to" (.getAbsolutePath bin-file))
-;;             (copy (.getInputStream jarfile zip-entry) bin-file)
-;;             (.setExecutable bin-file true)))))))
-
-
 (defn install
   "Install current project to the local repository."
   ([project]
      (let [jarfile (jar/jar project)
            pomfile (pom/pom project)]
-       ;; (install-shell-wrappers (JarFile. jarfile))
        (aether/install :coordinates [(symbol (:group project)
                                              (:name project))
                                      (:version project)]
                        :jar-file (io/file jarfile)
-                       :pom-file (io/file pomfile))))
-  ([_ project-name version]
-     (let [[name group] ((juxt name namespace) (symbol project-name))
-           temp-project (format "%s/lein-%s" (System/getProperty "java.io.tmpdir") (UUID/randomUUID))
-           jarfile (first (aether/resolve-dependencies
-                           :coordinates [[(symbol project-name) version]]
-                           :repositories (:repositories project/defaults)))]
-           ;; (install-shell-wrappers (JarFile. jarfile))
-       )))
+                       :pom-file (io/file pomfile)))))
