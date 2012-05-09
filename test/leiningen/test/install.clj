27346f0f628bae81832e570fa343504ed47dbaf6
Phil Hagelberg <technomancy@gmail.com>
2012-05-09 11:05:17 -0700

    Fix install test.

--- test/leiningen/test/install.clj
+++ test/leiningen/test/install.clj
@@ -5,52 +5,16 @@
         [leiningen.test.helper]
         [clojure.java.io :only [file]]))
 
-;; (def unix-shell-wrapper (file (user/leiningen-home) "bin" "nom"))
-;; (def windows-shell-wrapper (file (user/leiningen-home) "bin" "nom.bat"))
-
-;; (defn delete-shell-wrappers []
-;;   (.delete unix-shell-wrapper)
-;;   (.delete windows-shell-wrapper))
-
 (deftest test-install
   (delete-file-recursively (m2-dir "nomnomnom" "0.5.0-SNAPSHOT") true)
-  ;; (delete-shell-wrappers)
   (install sample-project)
-  (is (not (empty? (.listFiles (m2-dir "nomnomnom" "0.5.0-SNAPSHOT")))))
-  ;; (is (.exists unix-shell-wrapper))
-  ;; (if (= :windows (get-os))
-  ;;   (is (.exists windows-shell-wrapper))
-  ;;   (is (not (.exists windows-shell-wrapper))))
-  )
+  (is (not (empty? (.listFiles (m2-dir "nomnomnom" "0.5.0-SNAPSHOT"))))))
 
 (def jdom-dir (file local-repo "jdom" "jdom" "1.0"))
 
-(deftest ^:online test-standalone-install
-  (delete-file-recursively jdom-dir true)
-  ;; (delete-shell-wrappers)
-  (install nil "nomnomnom" "0.5.0-SNAPSHOT")
-  (is (not (empty? (.listFiles jdom-dir))))
-;;  (is (.exists unix-shell-wrapper))
-  )
-
 (def tricky-m2-dir (file local-repo "org" "domain" "tricky-name" "1.0"))
-;; (def tricky-unix-shell-wrapper (file (user/leiningen-home)
-;;                                      "bin" "tricky-name"))
-;; (def tricky-windows-shell-wrapper (file (user/leiningen-home)
-;;                                         "bin" "tricky-name.bat"))
-
-;; (defn delete-tricky-shell-wrappers []
-;;   (.delete tricky-unix-shell-wrapper)
-;;   (.delete tricky-windows-shell-wrapper))
 
 (deftest test-tricky-name-install
   (delete-file-recursively tricky-m2-dir true)
-  ;; (delete-shell-wrappers)
   (install tricky-name-project)
-  (install nil "org.domain/tricky-name" "1.0")
-  (is (not (empty? (.listFiles tricky-m2-dir))))
-  ;; (is (.exists tricky-unix-shell-wrapper))
-  ;; (if (= :windows (get-os))
-  ;;   (is (.exists tricky-windows-shell-wrapper))
-  ;;   (is (not (.exists tricky-windows-shell-wrapper))))
-  )
+  (is (not (empty? (.listFiles tricky-m2-dir)))))
