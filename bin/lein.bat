93bb6a1e146a674c6e91ea6aa5f01163073556ee
Phil Hagelberg <technomancy@gmail.com>
2012-05-11 18:46:25 -0700

    Bump back to 2.0.0-SNAPSHOT.

--- bin/lein.bat
+++ bin/lein.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set LEIN_VERSION=2.0.0-preview4
+set LEIN_VERSION=2.0.0-SNAPSHOT
 
 setLocal EnableExtensions EnableDelayedExpansion
 
--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 ;; This is Leiningen's own project configuration. See doc/TUTORIAL.md
 ;; file as well as sample.project.clj for help writing your own.
 
-(defproject leiningen "2.0.0-preview4"
+(defproject leiningen "2.0.0-SNAPSHOT"
   :description "Automate Clojure projects without setting your hair on fire."
   :url "https://github.com/technomancy/leiningen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[leiningen-core "2.0.0-preview4"]
+  :dependencies [[leiningen-core "2.0.0-SNAPSHOT"]
                  [clucy "0.2.3"]
                  [reply "0.1.0-beta6"]
                  [org.clojure/data.xml "0.0.3"]
@@ -42,4 +42,3 @@
 ;; * publish leiningen-core docs
 ;; * announce on mailing list
 ;; * bump version numbers back to snapshot
-;; * regenerate pom.xml
