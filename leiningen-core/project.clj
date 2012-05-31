0c6c58e32973f9d93cabac5833d47e7e1ce6c4d3
Phil Hagelberg <technomancy@gmail.com>
2012-05-30 21:07:18 -0700

    Fall back to ordered 1.2.0.

--- leiningen-core/project.clj
+++ leiningen-core/project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [classlojure "0.6.5"]
                  [robert/hooke "1.1.2"]
-                 [ordered "1.2.2"]
+                 [ordered "1.2.0"]
                  [com.cemerick/pomegranate "0.0.12"
                   :exclusions [org.slf4j/slf4j-api]]]
   :dev-resources-path "dev-resources"
