f10963d3e51e2cafe6244fa3288b6abe7dd5df7e
Phil Hagelberg <technomancy@gmail.com>
2012-06-22 10:24:54 -0700

    Pull in drawbridge directly so we can exclude ring.

--- project.clj
+++ project.clj
@@ -11,6 +11,8 @@
                  [org.clojure/data.xml "0.0.3"]
                  [bultitude "0.1.6"]
                  [reply "0.1.0-beta9"]
+                 ;; once reply bumps its drawbridge dep we can collapse these
+                 [com.cemerick/drawbridge "0.0.6" :exclusions [ring/ring-core]]
                  [clj-http "0.4.2"]]
   ;; checkout-deps don't work with :eval-in :leiningen
   :profiles {:dev {:resource-paths ["leiningen-core/dev-resources"]
