268bfbe6ebe58c3d6675da4868c11d886dfe193a
Phil Hagelberg <technomancy@gmail.com>
2012-06-24 22:06:07 -0700

    Fix test for lein-newnew.

--- test/leiningen/test/new.clj
+++ test/leiningen/test/new.clj
@@ -7,14 +7,14 @@
 (deftest test-new-with-just-project-name
   (leiningen.new/new nil "test-new-proj")
   (is (= #{"README.md" "project.clj" "src" "core.clj" "test"
-           "test_new_proj" "core_test.clj" ".gitignore"}
+           "doc" "intro.md" "test_new_proj" "core_test.clj" ".gitignore"}
          (set (map (memfn getName) (rest (file-seq (file "test-new-proj")))))))
   (delete-file-recursively (file "test-new-proj") :silently))
 
 (deftest test-new-with-group-and-project-name
   (leiningen.new/new nil "orgname/a-project")
   (is (= #{"README.md" "project.clj" "orgname" "src" "core.clj" "test"
-           "a_project" "core_test.clj" ".gitignore"}
+           "doc" "intro.md" "a_project" "core_test.clj" ".gitignore"}
          (set (map (memfn getName)
                    (rest (file-seq (file "a-project")))))))
   (delete-file-recursively (file "a-project") :silently))
