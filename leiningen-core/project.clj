390cbc675e529de7fb16b753bda4f2677a0a8b6f
Justin Balthrop <git@justinbalthrop.com>
2012-08-18 08:50:05 -0700

    Make :eval-in :classloader work	with native deps
    Use 0.6.6 classlojure's alter-java-library-path! to set java.library.path
    at runtime.
    
    Perhaps we should be setting all of these system properties back after
    we are done, including this one?

--- leiningen-core/project.clj
+++ leiningen-core/project.clj
@@ -5,7 +5,7 @@
   :description "Library for core functionality of Leiningen."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [bultitude "0.1.7"]
-                 [classlojure "0.6.5"]
+                 [classlojure "0.6.6"]
                  [robert/hooke "1.1.2"]
                  [ordered "1.3.1"]
                  [com.cemerick/pomegranate "0.0.13"
