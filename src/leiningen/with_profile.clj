787e76f3a8d4c9880ed981f0e769db5e24004f4d
Justin Balthrop <git@justinbalthrop.com>
2012-08-21 18:46:09 -0700

    rename reset-profiles to with-profiles

--- src/leiningen/with_profile.clj
+++ src/leiningen/with_profile.clj
@@ -6,7 +6,7 @@
   "Apply the given task with a comma-separated profile list."
   [project profiles task-name & args]
   (let [profiles (map keyword (.split profiles ","))
-        project (project/reset-profiles project profiles)
+        project (project/with-profiles project profiles)
         task-name (main/lookup-alias task-name project)]
     (main/apply-task task-name project args)))
 
