85c529ae3bc275bd665554d49f51ee7a643fd973
Phil Hagelberg <technomancy@gmail.com>
2012-08-25 16:39:25 -0700

    Fix warn-user-repos for new :repositories format.

--- leiningen-core/src/leiningen/core/project.clj
+++ leiningen-core/src/leiningen/core/project.clj
@@ -217,7 +217,7 @@
 
 (defn- warn-user-repos []
   (when (->> (vals (user/profiles))
-             (map (comp vals :repositories))
+             (map (comp second :repositories))
              (apply concat) (some :url))
     (println "WARNING: :repositories detected in user-level profile!")
     (println "See https://github.com/technomancy/leiningen/wiki/Repeatability")))
