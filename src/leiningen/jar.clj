5d37b41fbe1278ba4b696ac2938939ef5c69263a
Phil Hagelberg <technomancy@gmail.com>
2012-09-06 10:21:40 -0700

    Postel's law, I guess?

--- src/leiningen/jar.clj
+++ src/leiningen/jar.clj
@@ -115,6 +115,7 @@
   ([project uberjar?]
      (let [target (doto (io/file (:target-path project)) .mkdirs)
            suffix (if uberjar? "-standalone.jar" ".jar")
+           ;; TODO: splice in version to :jar-name
            jar-name (or (project (if uberjar? :uberjar-name :jar-name))
                         (str (:name project) "-" (:version project) suffix))]
        (str (io/file target jar-name))))
