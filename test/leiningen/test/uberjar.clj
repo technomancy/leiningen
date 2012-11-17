24ca286d546d0fc6db0ae0a2160f54ed638aafcf
Marshall T. Vandegrift <llasram@gmail.com>
2012-11-17 17:38:11 -0500

    Test :provided profile interpretation by the uberjar task.

--- test/leiningen/test/uberjar.clj
+++ test/leiningen/test/uberjar.clj
@@ -1,7 +1,8 @@
 (ns leiningen.test.uberjar
   (:use [leiningen.uberjar] :reload)
   (:use [clojure.test]
-        [leiningen.test.helper :only [sample-no-aot-project]])
+        [clojure.java.shell :only [sh]]
+        [leiningen.test.helper :only [sample-no-aot-project provided-project]])
   (:import [java.util.zip ZipFile]))
 
 (deftest test-uberjar
@@ -15,3 +16,10 @@
     (is (entries "nom/nom/nom.clj"))
     (is (entries "org/codehaus/janino/Compiler$1.class"))
     (is (not (some #(re-find #"dummy" %) entries)))))
+
+(deftest test-uberjar-provided
+  (let [-Xbootclasspath "-Xbootclasspath/a:leiningen-core/lib/clojure-1.4.0.jar"
+        filename "test_projects/provided/target/provided-0-standalone.jar"
+        _ (uberjar provided-project)]
+    (is (= 1 (:exit (sh "java" "-jar" filename))))
+    (is (= 0 (:exit (sh "java" -Xbootclasspath "-jar" filename))))))
---
+++ test_projects/provided/project.clj
@@ -0,0 +1,7 @@
+(defproject provided "0"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies []
+  :java-source-paths ["src"]
+  :main provided.core.Example
+  :profiles {:provided {:dependencies [[org.clojure/clojure "1.4.0"]]}})
