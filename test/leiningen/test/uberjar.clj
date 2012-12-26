f92f677eb799cbabda817578c411d9362f306efd
Phil Hagelberg <technomancy@gmail.com>
2012-12-26 10:12:03 -0800

    Disable a test that requires Java 7.

--- test/leiningen/test/uberjar.clj
+++ test/leiningen/test/uberjar.clj
@@ -1,12 +1,14 @@
 (ns leiningen.test.uberjar
-  (:use [leiningen.uberjar] :reload)
-  (:use [clojure.test]
-        [clojure.java.shell :only [sh]]
-        [leiningen.test.helper :only [sample-no-aot-project provided-project]])
-  (:import [java.util.zip ZipFile]))
+  (:require [leiningen.uberjar :refer :all]
+            [clojure.test :refer :all]
+            [clojure.java.shell :refer [sh]]
+            [leiningen.test.helper :refer [sample-no-aot-project
+                                           provided-project]])
+  (:import (java.util.zip ZipFile)))
 
 (deftest test-uberjar
-  (let [filename "test_projects/sample_no_aot/target/nomnomnom-0.5.0-SNAPSHOT-standalone.jar"
+  (let [filename (str "test_projects/sample_no_aot/target/"
+                      "nomnomnom-0.5.0-SNAPSHOT-standalone.jar")
         _ (uberjar sample-no-aot-project)
         entries (->> (ZipFile. filename)
                     .entries
@@ -17,9 +19,10 @@
     (is (entries "org/codehaus/janino/Compiler$1.class"))
     (is (not (some #(re-find #"dummy" %) entries)))))
 
-(deftest test-uberjar-provided
-  (let [-Xbootclasspath "-Xbootclasspath/a:leiningen-core/lib/clojure-1.4.0.jar"
+;; TODO: this breaks on Java 6
+(deftest ^:disabled test-uberjar-provided
+  (let [bootclasspath "-Xbootclasspath/a:leiningen-core/lib/clojure-1.4.0.jar"
         filename "test_projects/provided/target/provided-0-standalone.jar"
         _ (uberjar provided-project)]
     (is (= 1 (:exit (sh "java" "-jar" filename))))
-    (is (= 0 (:exit (sh "java" -Xbootclasspath "-jar" filename))))))
+    (is (= 0 (:exit (sh "java" bootclasspath "-jar" filename))))))
