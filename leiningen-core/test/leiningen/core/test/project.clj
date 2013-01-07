aaf5e2b56e01b83500c824c161072d8116ab8cb2
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-01-07 21:48:03 +0100

    Add a repo test case which actually merges repositories.

--- leiningen-core/test/leiningen/core/test/project.clj
+++ leiningen-core/test/leiningen/core/test/project.clj
@@ -118,6 +118,8 @@
           (make
            {:profiles {:clojars {:repositories ^:replace
                                 [["clojars.org" "https://clojars.org/repo/"]]}
+                       :clj-2 {:repositories
+                               [["clojars.org" "https://new-link.org/"]]}
                        :blue {:repositories
                               [["my-repo" "https://my-repo.org/"]]}
                        :empty {:repositories ^:replace []}}})]
@@ -135,6 +137,10 @@
       (is (= [["clojars.org" {:url "https://clojars.org/repo/"}]
               ["my-repo" {:url "https://my-repo.org/"}]]
              (-> (merge-profiles project [:clojars :blue])
+                 :repositories)))
+      (is (= [["clojars.org" {:url "https://new-link.org/"}]
+              ["my-repo" {:url "https://my-repo.org/"}]]
+             (-> (merge-profiles project [:clojars :blue :clj-2])
                  :repositories))))))
 
 (deftest test-global-exclusions
