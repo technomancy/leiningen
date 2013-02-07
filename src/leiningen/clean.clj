29340e8a6142974629214e79b553be3e063122d0
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-02-07 21:11:04 +0100

    Don't follow symlinks when cleaning. Fixes #988.

--- src/leiningen/clean.clj
+++ src/leiningen/clean.clj
@@ -1,14 +1,24 @@
 (ns leiningen.clean
   "Remove all files from project's target-path."
   (:require [clojure.java.io :as io]
-            [leiningen.core.eval :as eval]))
+            [leiningen.core.eval :as eval]
+            [leiningen.core.utils :as utils]))
+
+(defn real-directory?
+  "Returns true if this file is a real directory, false if it is a symlink or a
+  normal file."
+  [f]
+  (if (= :windows (eval/get-os))
+    (.isDirectory f)
+    (and (.isDirectory f)
+         (not (utils/symlink? f)))))
 
 (defn delete-file-recursively
   "Delete file f. If it's a directory, recursively delete all its contents.
 Raise an exception if any deletion fails unless silently is true."
   [f & [silently]]
   (let [f (io/file f)]
-    (when (.isDirectory f)
+    (when (real-directory? f)
       (doseq [child (.listFiles f)]
         (delete-file-recursively child silently)))
     (io/delete-file f silently)
