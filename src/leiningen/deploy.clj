technomancy/leiningen

commit a61d6a66a6b2a68b9b6761ac98509703b374532b

[#975] Deploy task should accept repo URL with credentials in it

src/leiningen/deploy.clj
@@ -5,6 +5,7 @@
             [leiningen.core.main :as main]
             [leiningen.core.eval :as eval]
             [leiningen.core.user :as user]
+            [leiningen.core.utils :as utils]
             [clojure.java.io :as io]
             [leiningen.pom :as pom]
             [leiningen.jar :as jar]
@@ -18,6 +19,14 @@
         (str message "\n" "See `lein help deploy` for an explanation of how to"
              " specify credentials.")
         :else message))
+
+(defn add-auth-from-url
+  [[id settings]]
+   (if-let [url (utils/build-url id)]
+     (let [user-info (.getUserInfo url)
+           [username password] (and user-info (.split user-info ":"))]
+      [id (assoc settings :username username :password password)])
+     [id settings]))
 
 (defn add-auth-interactively [[id settings]]
   (if (or (and (:username settings) (some settings [:password :passphrase
@@ -40,6 +49,7 @@
                          :when (= id name)] settings)]
     (-> [name settings]
         (classpath/add-repo-auth)
+        (add-auth-from-url)
         (add-auth-interactively))))
 
 (defn sign [file]

leiningen-core/src/leiningen/core/classpath.clj
@@ -4,9 +4,9 @@
             [cemerick.pomegranate :as pomegranate]
             [clojure.java.io :as io]
             [clojure.string :as str]
-            [leiningen.core.user :as user])
+            [leiningen.core.user :as user]
+            [leiningen.core.utils :as utils])
   (:import (java.util.jar JarFile)
-           (java.net URL)
            (org.sonatype.aether.resolution DependencyResolutionException)))
 
 ;; Basically just for re-throwing a more comprehensible error.
@@ -89,9 +89,7 @@
   ([] (get-proxy-settings "http_proxy"))
   ([key]
      (if-let [proxy (System/getenv key)]
-       (let [url (try (URL. proxy)
-                      (catch java.net.MalformedURLException _
-                        (URL. (str "http://" proxy))))
+       (let [url (utils/build-url proxy)
              user-info (.getUserInfo url)
              [username password] (and user-info (.split user-info ":"))]
          {:host (.getHost url)

leiningen-core/src/leiningen/core/utils.clj
@@ -1,6 +1,14 @@
 (ns leiningen.core.utils
   (:require [clojure.java.io :as io])
-  (:import [java.io File]))
+  (:import [java.io File]
+           [java.net URL]))
+
+(defn build-url
+  "Creates java.net.URL from string"
+  [url]
+   (try (URL. url)
+        (catch java.net.MalformedURLException _
+          (URL. (str "http://" url)))))
 
 (defn read-file
   "Read the contents of file if it exists."

commit 7dc109df88aa3e5ec0ef7e96e9e05408a28a8995

Fix repo credentials when no credentials in url
Credentials were being overwritten with nil when there was no username or
password in the repository url.  Fixes regression introduced in
a61d6a66a6b2a68b9b6761ac98509703b374532b.

src/leiningen/deploy.clj
@@ -22,11 +22,12 @@
 
 (defn add-auth-from-url
   [[id settings]]
-   (if-let [url (utils/build-url id)]
-     (let [user-info (.getUserInfo url)
-           [username password] (and user-info (.split user-info ":"))]
-      [id (assoc settings :username username :password password)])
-     [id settings]))
+  (let [url (utils/build-url id)
+        user-info (and url (.getUserInfo url))
+        [username password] (and user-info (.split user-info ":"))]
+    (if username
+      [id (assoc settings :username username :password password)]
+      [id settings])))
 
 (defn add-auth-interactively [[id settings]]
   (if (or (and (:username settings) (some settings [:password :passphrase