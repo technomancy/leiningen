ba5d6d9d60d3fbe76911134b3b911697b0b8841b
Michael Klishin <michael@defprotocol.org>
2013-03-04 04:02:18 +0400

    Make core templates use Clojure 1.5

--- resources/leiningen/new/default/project.clj
+++ resources/leiningen/new/default/project.clj
@@ -3,4 +3,4 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]])
