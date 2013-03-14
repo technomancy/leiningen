82d894b024f86375407ab564c76110294860d008
Phil Hagelberg <technomancy@gmail.com>
2013-03-14 16:17:01 -0700

    Document :java-cmd and improve update task's docstring.
    [ci skip]

--- src/leiningen/update.clj
+++ src/leiningen/update.clj
@@ -4,10 +4,14 @@
 (defn ^:higher-order update
   "Perform arbitrary transformations on your project map.
 
-Functions a lot like clojure.core/update-in with a few differences.
-Instead of a vector of keys, just mash them together like
-\":repl-options:port\". Provide the arguments to f followed by \"--\",
-and then the task name and arguments to the task."
+Acts a lot like calling `clojure.core/update-in` on your project map
+and then invoking a task on it, but with a few differences. Instead of
+a vector of keys for reaching into nested maps, just mash keywords
+together like \":repl-options:port\". Provide the arguments to f
+(which must be a resolvable var) followed by \"--\", and then the
+task name and arguments to the task:
+
+    $ lein update :dependencies conj \"[slamhound \\\"1.1.3\\\"]\" -- repl"
   [project keys f & args]
   (let [keys-vec (map keyword (rest (.split keys ":")))
         update-args (map read-string (take-while (partial not= "--") args))
