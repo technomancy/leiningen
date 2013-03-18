d27ec33cd76d73297f3936a703bb2e63207a7855
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-03-18 02:44:16 +0100

    credentials-fn != credentials

--- leiningen-core/test/leiningen/core/test/user.clj
+++ leiningen-core/test/leiningen/core/test/user.clj
@@ -3,16 +3,16 @@
         leiningen.core.user))
 
 (deftest resolving-repo-creds
-  (with-redefs [credentials-fn (constantly {#"^https://clojars\.org/.*"
-                                            {:username "u" :password "p"
-                                             :passphrase "looooong"
-                                             :private-key-file "./somewhere"}})]
+  (with-redefs [credentials (constantly {#"^https://clojars\.org/.*"
+                                         {:username "u" :password "p"
+                                          :passphrase "looooong"
+                                          :private-key-file "./somewhere"}})]
     (testing "Literal creds unmolested"
       (is (= (resolve-credentials {:url "https://clojars.org/repo"
                                    :username "easily" :password "stolen"})
              {:url "https://clojars.org/repo"
               :username "easily" :password "stolen"})))
-    (testing "Lookup in enivronment"
+    (testing "Lookup in environment"
       (with-redefs [getenv {"LEIN_USERNAME" "flynn"
                             "CUSTOMENV" "flotilla"}]
         (is (= (resolve-credentials {:url "https://clojars.org/repo"
