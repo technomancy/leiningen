dba0815138c85fc9f7b4e9134d7c7044bd7da51a
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-03-26 03:04:44 +0100

    Add guard rails for `lein new` newline handling.

--- test/leiningen/test/new/templates.clj
+++ test/leiningen/test/new/templates.clj
@@ -2,7 +2,41 @@
   (:use clojure.test
         leiningen.new.templates)
   (:require [leiningen.test.helper :refer [abort-msg]]
+            [leiningen.core.user :as user]
             [clojure.java.io :as io]))
+
+(defn- getenv [s]
+  (System/getenv s))
+
+(deftest line-separators
+  (testing "that nothing changes when we're on unix systems"
+    (with-redefs [user/getprop (constantly "\n")]
+      (is (= (fix-line-separators "foo") "foo"))
+      (is (= (fix-line-separators "bar\nbaz") "bar\nbaz"))
+      (is (= (fix-line-separators "quux\n\n\nsycorax") "quux\n\n\nsycorax"))))
+
+  (testing "that newlines are correctly converted on '\\r\\n' systems"
+    (with-redefs [user/getprop (constantly "\r\n")]
+      (is (= (fix-line-separators "foo") "foo"))
+      (is (= (fix-line-separators "bar\nbaz") "bar\r\nbaz"))
+      (is (= (fix-line-separators "quux\n\n\nsycorax")
+             "quux\r\n\r\n\r\nsycorax"))))
+
+  (testing "that other bizarre systems get same treatment"
+    (with-redefs [user/getprop (constantly "\t\t")]
+      (is (= (fix-line-separators "foo") "foo"))
+      (is (= (fix-line-separators "bar\nbaz") "bar\t\tbaz"))
+      (is (= (fix-line-separators "quux\n\n\nsycorax")
+             "quux\t\t\t\t\t\tsycorax"))))
+
+  (testing "that one can override the normal system newline"
+    (with-redefs [user/getprop (constantly "\r\n")
+                  user/getenv (fn [s] (if (= s "LEIN_NEW_UNIX_NEWLINES")
+                                      "y"
+                                      (getenv s)))]
+      (is (= (fix-line-separators "foo") "foo"))
+      (is (= (fix-line-separators "bar\nbaz") "bar\nbaz"))
+      (is (= (fix-line-separators "quux\n\n\nsycorax") "quux\n\n\nsycorax")))))
 
 (deftest project-names
   (is (= (project-name "org.example/foo.bar") "foo.bar"))
