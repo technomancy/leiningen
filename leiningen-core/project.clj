10d7d1faa33df68066202863cc6439e8fe49295a
Phil Hagelberg <technomancy@gmail.com>
2013-03-28 14:46:32 -0700

    Release 2.1.2

--- leiningen-core/project.clj
+++ leiningen-core/project.clj
@@ -1,4 +1,4 @@
-(defproject leiningen-core "2.2.0-SNAPSHOT"
+(defproject leiningen-core "2.1.2"
   :url "https://github.com/technomancy/leiningen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
