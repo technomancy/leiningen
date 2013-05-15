cbff7f06fb921bffe6368d3d812c6391ac444900
Phil Hagelberg <technomancy@gmail.com>
2013-05-15 13:53:01 -0700

    Fix -o and -U aliases to be additive.

--- leiningen-core/src/leiningen/core/main.clj
+++ leiningen-core/src/leiningen/core/main.clj
@@ -10,9 +10,8 @@
 (def aliases {"-h" "help", "-help" "help", "--help" "help", "-?" "help",
               "-v" "version", "-version" "version", "--version" "version",
               "überjar" "uberjar",
-              ;; TODO: these should use add-profile instead
-              "-o" ["with-profile" "offline,dev,user,default"]
-              "-U" ["with-profile" "update,dev,user,default"]
+              "-o" ["with-profile" "+offline"]
+              "-U" ["with-profile" "+update"]
               "cp" "classpath" "halp" "help"
               "with-profiles" "with-profile"
               "readme" ["help" "readme"]
