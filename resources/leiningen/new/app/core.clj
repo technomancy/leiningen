17f99c2f91efeb03b3443df7aef2817b4d59f07b
Phil Hagelberg <technomancy@gmail.com>
2013-06-14 11:39:34 -0700

    Don't need this now with read-edn.

--- resources/leiningen/new/app/core.clj
+++ resources/leiningen/new/app/core.clj
@@ -4,6 +4,4 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  ;; work around dangerous default behaviour in Clojure
-  (alter-var-root #'*read-eval* (constantly false))
   (println "Hello, World!"))
