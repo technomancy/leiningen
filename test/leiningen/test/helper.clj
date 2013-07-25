4feade3f7b8c5e4359f9242ca80f48d5302a5b2c
Phil Hagelberg <technomancy@gmail.com>
2013-07-24 19:21:23 -0700

    Fix wonky newlines.

--- test/leiningen/test/helper.clj
+++ test/leiningen/test/helper.clj
@@ -61,20 +61,19 @@ Raise an exception if any deletion fails unless silently is true."
   (clojure.string/replace input-str "/" java.io.File/separator))
 
 ;so paths would work under windows too which adds a drive letter and changes the path separator
-(defn pathify 
+(defn pathify
   "
 pass only absolute paths, will throw if not
 because if not absolute then .getAbsolutePath will resolve them relative to current directory
   "
   [in-str-or-file]
-  (cond (or 
+  (cond (or
           (nil? in-str-or-file)
           (not (or
                  (.startsWith in-str-or-file "/")
-                 (and 
+                 (and
                    (>= (.length in-str-or-file) 3)
                    (= ":\\" (.substring in-str-or-file 1 3))))))
     (throw (new RuntimeException (str "bad usage, passed: `" in-str-or-file "`")))
     :else
     (.getAbsolutePath (io/as-file in-str-or-file))))
-
