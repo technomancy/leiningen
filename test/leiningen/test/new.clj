41207ad7269481f413ec48276aa163ebf4cae201
Wolodja Wentland <babilen@gmail.com>
2013-07-30 15:07:44 +0100

    Add LICENSE file to project templates
    Projects created by "lein new" are meant to be licensed under the EPL as noted
    in project.clj and README.md. One requirement of the EPL is, however, that "a
    copy of this Agreement [the EPL] must be included with each copy of the
    Program." [0].
    
    This means that projects created by leiningen cannot be legally distributed if
    the author fails to add the LICENSE him/herself. This patch fixes this issue
    by creating a LICENSE file with a plain text copy of the EPL 1.0 (cf. [0]) by
    default for new projects.
    
    [0] http://www.eclipse.org/legal/epl-v10.html → 3. REQUIREMENTS

--- test/leiningen/test/new.clj
+++ test/leiningen/test/new.clj
@@ -7,14 +7,16 @@
 (deftest test-new-with-just-project-name
   (leiningen.new/new nil "test-new-proj")
   (is (= #{"README.md" "project.clj" "resources" "src" "core.clj" "test"
-           "doc" "intro.md" "test_new_proj" "core_test.clj" ".gitignore"}
+           "doc" "intro.md" "test_new_proj" "core_test.clj" ".gitignore"
+           "LICENSE"}
          (set (map (memfn getName) (rest (file-seq (file "test-new-proj")))))))
   (delete-file-recursively (file "test-new-proj") :silently))
 
 (deftest test-new-with-group-and-project-name
   (leiningen.new/new nil "orgname/a-project")
   (is (= #{"src" "a_project_test.clj" "project.clj" "a_project.clj" "orgname"
-           "resources" "test" ".gitignore" "README.md" "doc" "intro.md"}
+           "resources" "test" ".gitignore" "README.md" "doc" "intro.md"
+           "LICENSE"}
          (set (map (memfn getName)
                    (rest (file-seq (file "a-project")))))))
   (delete-file-recursively (file "a-project") :silently))
@@ -22,27 +24,31 @@
 (deftest test-new-with-explicit-default-template
   (leiningen.new/new nil "default" "test-new-proj")
   (is (= #{"README.md" "project.clj" "src" "core.clj" "test" "resources"
-           "doc" "intro.md" "test_new_proj" "core_test.clj" ".gitignore"}
+           "doc" "intro.md" "test_new_proj" "core_test.clj" ".gitignore"
+           "LICENSE"}
          (set (map (memfn getName) (rest (file-seq (file "test-new-proj")))))))
   (delete-file-recursively (file "test-new-proj") :silently))
 
 (deftest test-new-with-app-template
   (leiningen.new/new nil "app" "test-new-app")
   (is (= #{"README.md" "project.clj" "src" "core.clj" "test" "resources"
-           "doc" "intro.md" "test_new_app" "core_test.clj" ".gitignore"}
+           "doc" "intro.md" "test_new_app" "core_test.clj" ".gitignore"
+           "LICENSE"}
          (set (map (memfn getName) (rest (file-seq (file "test-new-app")))))))
   (delete-file-recursively (file "test-new-app") :silently))
 
 (deftest test-new-with-plugin-template
   (leiningen.new/new nil "plugin" "test-new-plugin")
-  (is (= #{"README.md" "project.clj" "src" "leiningen" "test_new_plugin.clj" ".gitignore"}
+  (is (= #{"README.md" "project.clj" "src" "leiningen" "test_new_plugin.clj" ".gitignore"
+           "LICENSE"}
          (set (map (memfn getName) (rest (file-seq (file "test-new-plugin")))))))
   (delete-file-recursively (file "test-new-plugin") :silently))
 
 (deftest test-new-with-template-template
   (leiningen.new/new nil "template" "test-new-template")
   (is (= #{"README.md" "project.clj" "src" "leiningen" "new"
-           "test_new_template.clj" "test_new_template" "foo.clj" ".gitignore"}
+           "test_new_template.clj" "test_new_template" "foo.clj" ".gitignore"
+           "LICENSE"}
          (set (map (memfn getName) (rest (file-seq (file "test-new-template")))))))
   (delete-file-recursively (file "test-new-template") :silently))
 
@@ -73,7 +79,8 @@
 (deftest test-new-with-to-dir-option
   (leiningen.new/new nil "test-new-proj" "--to-dir" "my-proj")
   (is (= #{"README.md" "project.clj" "src" "core.clj" "test" "resources"
-           "doc" "intro.md" "test_new_proj" "core_test.clj" ".gitignore"}
+           "doc" "intro.md" "test_new_proj" "core_test.clj" ".gitignore"
+           "LICENSE"}
          (set (map (memfn getName) (rest (file-seq (file "my-proj")))))))
   (delete-file-recursively (file "my-proj") :silently))
 
@@ -87,7 +94,8 @@
 
     (leiningen.new/new nil "test-new-proj")
     (is (= #{"README.md" "project.clj" "src" "core.clj" "test" "resources"
-             "doc" "intro.md" "test_new_proj" "core_test.clj" ".gitignore"}
+             "doc" "intro.md" "test_new_proj" "core_test.clj" ".gitignore"
+             "LICENSE"}
            (set (map (memfn getName) (rest (file-seq (file new-pwd "test-new-proj")))))))
     (System/setProperty "leiningen.original.pwd" original-pwd)
     (delete-file-recursively (file new-pwd "test-new-proj") :silently)))
