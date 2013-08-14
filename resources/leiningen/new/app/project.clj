e8121568d2c43a6c00fedd506edbc71531769d40
Phil Hagelberg <technomancy@gmail.com>
2013-08-13 18:52:53 -0700

    Add :aot :all to uberjar profile of app template.

--- resources/leiningen/new/app/project.clj
+++ resources/leiningen/new/app/project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :main {{namespace}})
+  :main {{namespace}}
+  :profiles {:uberjar {:aot :all}})
