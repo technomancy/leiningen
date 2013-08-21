0a5c616bbd350634844f7176087a85617bee9435
Phil Hagelberg <technomancy@gmail.com>
2013-08-20 17:42:27 -0700

    Bump version up to 2.3.3-SNAPSHOT.

--- leiningen-core/project.clj
+++ leiningen-core/project.clj
@@ -1,4 +1,4 @@
-(defproject leiningen-core "2.3.2"
+(defproject leiningen-core "2.3.3-SNAPSHOT"
   :url "https://github.com/technomancy/leiningen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -11,13 +11,7 @@
                  [org.apache.maven.wagon/wagon-http "2.4"]
                  [com.hypirion/io "0.3.1"]
                  [pedantic "0.1.0"]]
-  ;; until the pomegratate snapshot is released:
-  :repositories [["sonatype"
-                  "https://oss.sonatype.org/content/repositories/snapshots/"]]
   :scm {:dir ".."}
-  ;; This is only used when releasing Leiningen. Can't put it in a
-  ;; profile since it must be installed using lein1
-  ;;:aot :all
   :dev-resources-path "dev-resources"
   :aliases {"bootstrap" ["with-profile" "base"
                          "do" "install," "classpath" ".lein-bootstrap"]})
