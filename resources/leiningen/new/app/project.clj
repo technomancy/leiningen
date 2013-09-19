d3facea5a2f022907efce5ba443e468a107eafbe
Phil Hagelberg <technomancy@gmail.com>
2013-09-19 10:39:25 -0700

    Use profile-scoped targets in app template.

--- resources/leiningen/new/app/project.clj
+++ resources/leiningen/new/app/project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :main ^:skip-aot {{namespace}}
+  :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
