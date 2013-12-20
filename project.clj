248362f2e72973eeeab8cb85e8ed1657b751e4fb
Phil Hagelberg <technomancy@gmail.com>
2013-12-20 15:25:54 -0800

    Upgrade stencil and exclude core.cache.
    Should help avoid conflicts with dependencies that need a different version.

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                  [org.clojure/data.xml "0.0.3"]
                  [commons-io "2.4"]
                  [bultitude "0.2.2"]
-                 [stencil "0.3.2"]
+                 [stencil "0.3.3" :exclusions [org.clojure/core.cache]]
                  [org.apache.maven.indexer/indexer-core "4.1.3"
                   :exclusions [org.apache.maven/maven-model
                                org.sonatype.aether/aether-api
