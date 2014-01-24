0b5e0d204c2296292f1df8e6f2e00c5d89d28128
Max Prokopiev <me@maxprokopiev.com>
2014-01-24 09:33:04 +0400

    Add license key in other places
    (#1419)

--- lein-pprint/project.clj
+++ lein-pprint/project.clj
@@ -1,3 +1,5 @@
 (defproject lein-pprint "1.1.1"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Pretty-print a representation of the project map."
   :eval-in-leiningen true)
