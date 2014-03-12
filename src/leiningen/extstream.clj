fdcaddebbea82b73fa1e04a0bd2715c97bfc23c5
Matthew Blair <mpblair@gmail.com>
2014-03-12 19:04:03 -0400

    Finishing touches

--- src/leiningen/extstream.clj
+++ src/leiningen/extstream.clj
@@ -1,35 +1,50 @@
 (ns leiningen.extstream
+  "Extends java.io.InputStream by reporting to console output
+the percentage of the stream that has been consumed after each read."
   (:gen-class
    :extends java.io.InputStream
    :state state
    :init init
    :constructors {[java.io.InputStream Long] []}
+   :methods [[saveTotalBytes [Long] void]
+             [printProgress [] void]]
    :main false))
 
 (defn -init [stream content-length]
-  [ [] (ref (into {} {:stream stream
+  [ [] (atom (into {} {:stream stream
                       :content-length content-length
                       :total-bytes 0})) ])
 
-(defn print-progress [count length]
-  (let [progress (/ (* count 100.0) length)]
-    (printf "%.1f%%\r" progress)))
 
-(defn -read-void [this]
-  (let [b []
-        count (.read (:stream @(.state this)) b)]
-    (if (= -1 count) count (first b))))
+(defn- -saveTotalBytes [this total-bytes ]
+  (let [state-map (.state this)]
+    (reset! state-map (assoc @state-map
+                        :total-bytes total-bytes))))
+
+
+(defn- -printProgress [this]
+  (let [state @(.state this)
+        total-bytes (:total-bytes state)
+        content-length (:content-length state)
+        progress (/ (* total-bytes 100.0) content-length)
+        ending (if-not (== progress 100) "\r" "\n")]
+    (printf "%.1f%% complete%s" progress ending)
+    (flush)))
+
 
 (defn -read-byte<> [this bytebuf]
-  (let [state-map (.state this)
-        stream (:stream @state-map)
-        content-length (:content-length @state-map)
+  (let [state @(.state this)
+        stream (:stream state)
         count (.read stream bytebuf)]
+    (-saveTotalBytes this (+ (:total-bytes state) count))
+    (-printProgress this)
     count))
 
+
 (defn -read-byte<>-int-int [this bytebuf off len]
-  (let [state-map (.state this)
-        stream (:stream @state-map)
-        content-length (:content-length @state-map)
+  (let [state @(.state this)
+        stream (:stream state)
         count (.read stream bytebuf off len)]
+    (-saveTotalBytes this (+ (:total-bytes state) count))
+    (-printProgress this)
     count))
