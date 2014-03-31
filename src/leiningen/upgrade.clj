f6591bd660556a82b44c8c4ca2cd88383cb205c8
Jean Niklas L'orange <jeannikl@hypirion.com>
2014-03-31 13:25:15 +0200

    Print error message when `upgrade` is unavailable.
    Closes #1481.

--- src/leiningen/upgrade.clj
+++ src/leiningen/upgrade.clj
@@ -1,9 +1,13 @@
 (ns leiningen.upgrade
-  "Upgrade Leiningen to specified version or latest stable.")
+  "Upgrade Leiningen to specified version or latest stable."
+  (:require [leiningen.core.main :as main]))
 
 ;; This file is only a placeholder. The real upgrade
 ;; implementation can be found in the 'lein' script.
 
-(defn upgrade
+(defn ^:no-project-needed upgrade
   "Upgrade Leiningen to specified version or latest stable."
-  ([]) ([target-version]))
+  [project & args]
+  (main/abort "Upgrade is either disabled, or you have tried to call it from a"
+              "higher order\ntask. If you're using lein from a package manager,"
+              "upgrade lein through the\npackage manager's install commands."))
