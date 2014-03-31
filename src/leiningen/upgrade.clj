2d8a871df05ef64ae9228685859f7a66ad7372d6
Jean Niklas L'orange <jeannikl@hypirion.com>
2014-03-31 13:28:55 +0200

    Reword error message from `lein upgrade`.

--- src/leiningen/upgrade.clj
+++ src/leiningen/upgrade.clj
@@ -9,5 +9,6 @@
   "Upgrade Leiningen to specified version or latest stable."
   [project & args]
   (main/abort "Upgrade is either disabled, or you have tried to call it from a"
-              "higher order\ntask. If you're using lein from a package manager,"
-              "upgrade lein through the\npackage manager's install commands."))
+              "higher order\ntask. If you've installed lein through a package"
+              "manager, upgrade lein through\nthe package manager's upgrade"
+              "commands."))
