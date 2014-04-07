579499b5e70f1bb8292ce61105c9b577656eda1c
Jean Niklas L'orange <jeannikl@hypirion.com>
2014-04-08 00:23:27 +0200

    Depend on latest Pomegranate, fixes #1447.

--- leiningen-core/project.clj
+++ leiningen-core/project.clj
@@ -7,7 +7,7 @@
                  [bultitude "0.2.6"]
                  [classlojure "0.6.6"]
                  [robert/hooke "1.3.0"]
-                 [com.cemerick/pomegranate "0.2.0"]
+                 [com.cemerick/pomegranate "0.3.0"]
                  [org.apache.maven.wagon/wagon-http "2.4"]
                  [com.hypirion/io "0.3.1"]
                  [pedantic "0.1.0"]]
