88181a4bbfc992c3a1b7c78a60b8fbb9b2dd0ae6
Phil Hagelberg <technomancy@gmail.com>
2014-04-22 15:03:34 -0700

    Use dep-key for distinct call in pom task.
    group/artifact version tuple isn't enough to de-dupe correctly.
    
    Fixes #1494.

--- test_projects/native/project.clj
+++ test_projects/native/project.clj
@@ -4,6 +4,8 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [serial-port "1.0.7"]
                  [penumbra/lwjgl "2.4.2"]
+                 [com.badlogicgames.gdx/gdx-platform "0.9.9"]
+                 [com.badlogicgames.gdx/gdx-platform "0.9.9" :classifier "natives-desktop"]
                  [org.clojars.samaaron/rxtx "2.2.0"]
                  [jriengine "0.8.4"]
                  [tokyocabinet "1.24.0"]])
