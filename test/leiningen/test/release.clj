405fa314836d869d13ca14afcaa9d6afb5c76e6f
Phil Hagelberg <technomancy@gmail.com>
2014-05-24 14:31:37 -0700

    Fix release tests.

--- test/leiningen/test/release.clj
+++ test/leiningen/test/release.clj
@@ -15,40 +15,38 @@
     :minor 0
     :patch 0
     :qualifier nil}
-   "1.0.0"
-   {:major "2.0.0"
-    :minor "1.1.0"
-    :patch "1.0.1"}]
+   {:major "2.0.0-SNAPSHOT"
+    :minor "1.1.0-SNAPSHOT"
+    :patch "1.0.1-SNAPSHOT"}]
 
    ["1.2.3"
    {:major 1
     :minor 2
     :patch 3
     :qualifier nil}
-   "1.2.3"
-   {:major "2.0.0"
-    :minor "1.3.0"
-    :patch "1.2.4"}]
+   {:major "2.0.0-SNAPSHOT"
+    :minor "1.3.0-SNAPSHOT"
+    :patch "1.2.4-SNAPSHOT"}]
 
    ["1.2.3-herp"
    {:major 1
     :minor 2
     :patch 3
     :qualifier "herp"}
-   "1.2.3"
-   {:major "2.0.0"
-    :minor "1.3.0"
-    :patch "1.2.4"}]
+   {:major "2.0.0-SNAPSHOT"
+    :minor "1.3.0-SNAPSHOT"
+    :patch "1.2.4-SNAPSHOT"
+    :release "1.2.3"}]
 
    ["1.0.0-SNAPSHOT"
    {:major 1
     :minor 0
     :patch 0
     :qualifier "SNAPSHOT"}
-   "1.0.0"
-   {:major "2.0.0"
-    :minor "1.1.0"
-    :patch "1.0.1"}]])
+   {:major "2.0.0-SNAPSHOT"
+    :minor "1.1.0-SNAPSHOT"
+    :patch "1.0.1-SNAPSHOT"
+    :release "1.0.0"}]])
 
 (deftest test-parse-semver-version
   (testing "Testing semantic version string parsing"
@@ -65,17 +63,8 @@
               (parse-semantic-version (first semver-test-data))))))))
 
 (deftest version-map->string-valid
-  (doseq [semver-test-data valid-semver-version-values]
-    (is (= (nth semver-test-data 2)
-           (version-map->string (second semver-test-data))))))
-
-(deftest test-bump-version
-  (testing "Testing semantic version increment"
-    (doseq [semver-test-data valid-semver-version-values]
-      (testing (format "with valid version: %s\n"
-                       semver-test-data)
-        (doseq [[k v] (map identity (nth semver-test-data 3))]
-          (testing (format "version-level %s" (name k))
-            (is (= v (version-map->string
-                       (bump-version
-                         (nth semver-test-data 1) k))))))))))
+  (doseq [[string parsed bumps] valid-semver-version-values]
+    (is (= string (version-map->string parsed)))
+    (doseq [[level string] bumps]
+      (is (= (merge {:qualifier nil} (bump-version level parsed))
+             (parse-semantic-version string))))))
