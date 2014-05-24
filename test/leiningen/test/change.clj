4dc50a6d027b1792681f20ad8fdaa818eaf7b698
Chris Truter <jeffpalentine@gmail.com>
2014-05-24 05:33:12 +0200

    Attempt to make types transparent across sjacket boundary
    Not sure how robust this is, but looks like a step towards less annoyance / surprise

--- test/leiningen/test/change.clj
+++ test/leiningen/test/change.clj
@@ -14,35 +14,29 @@
     (is (thrown-with-msg?
          IllegalArgumentException #"Project definition not found"
          (change* ";;(defproject stealth.library \"0.0.0\")"
-                  :version :reset-str "0.0.1"))))
+                  :version :reset "0.0.1"))))
 
   (testing "project version not found"
     (is (thrown-with-msg?
          IllegalArgumentException #"Project version not found"
          (change* "(defproject com.someproject :dependencies [[\"some.thing\" \"2.3.1\"]])"
-                  :version :reset-str "1.2.3"))))
+                  :version :reset "1.2.3"))))
 
   (testing "simplest possible case"
     (is (= "(defproject leingingen.change \"0.0.2-SNAPSHOT\")"
            (change* "(defproject leingingen.change \"0.0.1\")"
-                    :version :reset-str "0.0.2-SNAPSHOT"))))
+                    :version :reset "0.0.2-SNAPSHOT"))))
 
   (testing "the largest project.clj in the repo"
     (let [before (slurp (clojure.java.io/resource "leiningen/help/project.clj"))
-          after  (change* before :version :reset-str "6.4.1")]
+          after  (change* before :version :reset "6.4.1")]
       ;; check the key portion
       (is (= "(defproject org.example/sample \"6.4.1\" " (.substring after 529 568)))
       ;; check a random dependency for changes
       (is (= "log4j \"1.2.15\"" (.substring after 2572 2586))))))
 
-(deftest test-bump-version
-  (testing "simplest possible case"
-    (is (= "(defproject leingingen.change \"1.9.53-SNAPSHOT\")"
-           (change* "(defproject leingingen.change \"1.9.52-SNAPSHOT\")"
-                    :version :bump-version)))))
-
 (deftest test-external-function
-  (testing "simplest possible case"
+  (testing "regular function"
     (is (= "(defproject leingingen.change \"1.9.53-SNAPSHOT\")"
            (change* "(defproject leingingen.change \"1.9.52-SNAPSHOT\")"
-                    :version run-bump-version)))))
+                    :version :swap bump-version)))))
