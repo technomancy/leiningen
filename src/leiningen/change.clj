c071b8bd37af4927052085ed09c80662e319b4fc
Chris Truter <jeffpalentine@gmail.com>
2014-05-24 04:57:26 +0200

    Improve interface for changing project settings

--- src/leiningen/change.clj
+++ src/leiningen/change.clj
@@ -10,15 +10,24 @@
 
 (defn- unwrap-string [[_ str _]] str)
 
+(defn- lookup-var [prefix task-name]
+  (->> task-name
+       name
+       (str "leiningen.change/" prefix "-")
+       symbol
+       find-var))
+
+(defn- fail-argument! [msg]
+  (throw (IllegalArgumentException. msg)))
+
+;;-- operations
+
 (defn- bump-version [version]
   ;; NOTE: technically http://semver.org/ defines 'prelease' and 'meta' data
   ;; TODO: better error handling here (wrong structure? not a number?)
   (let [[major minor patch meta] (str/split version #"\.|\-")
         new-patch (inc (Long/parseLong patch))]
     (format "%s.%s.%d-%s" major minor new-patch meta)))
-
-(defn- fail-argument! [msg]
-  (throw (IllegalArgumentException. msg)))
 
 ;;-- traversal
 
@@ -59,34 +68,27 @@
 
 ;;-- tasks
 
-(defn- run-version
-  "Replace the project version"
-  [project-str & [version]]
-  (swap-version project-str assoc :content (wrap-string version)))
+(defn- run-reset-str [target value]
+  (assoc target :content (wrap-string value)))
 
-(defn- run-bump-version
-  "Update the project version's patch number"
-  [project-str]
-  (swap-version project-str update-in [:content] (comp wrap-string
-                                                       bump-version
-                                                       unwrap-string)))
+(defn- run-swap-str [target fn & args]
+  (update-in target [:content] (comp wrap-string fn unwrap-string)))
 
-(defn- lookup-task [task-name]
-  (->> task-name
-       name
-       (str "leiningen.change/run-")
-       symbol
-       find-var))
+(defn run-bump-version [target]
+  (run-swap-str target bump-version))
+
+;;-- public API
 
 (defn change*
-  [project-str task & args]
-  (if-let [task-fn (lookup-task task)]
-    (apply task-fn project-str args)
-    (fail-argument! "Only support changing :version for now")))
+  [project-str key & [fn & args]]
+  (if-let [swap-key (lookup-var "swap" key)]
+    (let [fn' (if (fn? fn) fn (lookup-var "run" fn))]
+      (apply swap-key project-str fn' args))
+    (fail-argument! (str "Do not currently support changing :" (name key)))))
 
 (defn change
   "Rewrite project.clj with new settings"
-  [project task & args]
+  [project key & args]
   ;; cannot work with project, as want to preserve formatting, comments, etc
   (let [source (slurp "project.clj")]
-    (spit "project.clj" (apply change* source task args))))
+    (spit "project.clj" (apply change* source key args))))
