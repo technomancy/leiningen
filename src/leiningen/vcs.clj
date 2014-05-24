47dc18aafa8746482d58bf10c97f95c4d4c192d1
Phil Hagelberg <technomancy@gmail.com>
2014-05-23 20:58:13 -0700

    Add assert-committed method to leiningen.vcs.

--- src/leiningen/vcs.clj
+++ src/leiningen/vcs.clj
@@ -3,7 +3,7 @@
             [leiningen.core.eval :as eval]
             [leiningen.core.main :as main]))
 
-;; TODO: make pom task use this ns
+;; TODO: make pom task use this ns by adding a few more methods
 
 (def supported-systems (atom [:git]))
 
@@ -14,20 +14,34 @@
 (defn which-vcs [project & _]
   (some (partial uses-vcs project) @supported-systems))
 
+
+;;; Methods
+
 (defmulti push "Push to your remote repository." which-vcs :default :none)
+
+(defmulti tag "Apply a version control tag." which-vcs)
+
+(defmulti assert-committed "Abort if uncommitted changes exist." which-vcs)
+
+
+;;; Git
 
 (defmethod push :git [project]
   (binding [eval/*dir* (:root project)]
     (eval/sh "git" "push")
     (eval/sh "git" "push" "--tags")))
 
-(defmulti tag "Apply a version control tag" which-vcs)
-
 (defmethod tag :git [project version]
   (binding [eval/*dir* (:root project)]
     (eval/sh "git" "tag" "-s" version "-m" (str "Release " version))))
 
-;; TODO: add a subtask to assert there are no uncommitted changes
+(defmethod assert-committed :git [project]
+  (binding [eval/*dir* (:root project)]
+    (when-not (re-find #"working directory clean" (with-out-str
+                                                    (eval/sh "git" "status")))
+      (main/abort "Uncommitted changes in" (:root project) "directory."))))
+
+
 
 (defn- not-found [subtask]
   (partial #'main/task-not-found (str "vcs " subtask)))
