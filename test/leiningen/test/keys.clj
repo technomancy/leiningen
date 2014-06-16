be8b29ae2c8c76e79b50da360b4910fb2d4629ee
Phil Hagelberg <technomancy@gmail.com>
2014-06-15 18:30:32 -0700

    Update new key expiry dates.

--- test/leiningen/test/keys.clj
+++ test/leiningen/test/keys.clj
@@ -2,11 +2,11 @@
   (:require [clojure.test :refer :all]))
 
 (deftest technomancy-gpg-key
-  (let [month-before-key-expiry #inst "2014-06-16"]
+  (let [month-before-key-expiry #inst "2017-05-14"]
     (is (< (System/currentTimeMillis) (.getTime month-before-key-expiry))
         "If this fails, yell at technomancy to generate a new key!")))
 
 (deftest clojars-ssl-cert
-  (let [month-before-cert-expiry #inst "2014-10-09"]
+  (let [month-before-cert-expiry #inst "2016-07-12"]
     (is (< (System/currentTimeMillis) (.getTime month-before-cert-expiry))
         "If this fails, yell at _ato to get a new SSL cert.")))
