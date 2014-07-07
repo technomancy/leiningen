94601082e417bc67946088efa317c5c83834b08f
Gary Fredericks <fredericksgary@gmail.com>
2014-07-07 13:28:43 -0500

    Fix the test that's been failing on Travis
    The problem was something to do with the project map being too
    bare. Switching to `defproject` fixes it.

--- leiningen-core/test/leiningen/core/test/mirrors.clj
+++ leiningen-core/test/leiningen/core/test/mirrors.clj
@@ -1,20 +1,23 @@
 (ns leiningen.core.test.mirrors
   (:require [clojure.test :refer :all]
             [cemerick.pomegranate :as pom]
-            [leiningen.core.project :refer [init-project]]))
+            [leiningen.core.project :refer [defproject init-project]]))
 
-;; Regression test for issue #1555
+
+;; Regression test for Issue #1555
+(defproject mirrors-work-ok-with-plugins-project "0.0.0"
+  ;; we need to use a sequence of pairs rather than a map to
+  ;; reproduce the bug; IRL maps got converted to seqs somehow or
+  ;; another anyhow, but apparently not by init-project.
+  :mirrors [["central" {:name "foo"
+                        :url "http://repo1.maven.org/maven2/"}]]
+  ;; Have to have a plugin to reproduce
+  :plugins [[lein-pprint "1.1.1"]])
+
 (deftest ^:online mirrors-work-ok-with-plugins
   ;; turn off add-classpath so we don't actually mutate the classpath;
   ;; we're still hitting the internet, and there's probably something
   ;; in pomegranate we could redef to prevent that, but I haven't
   ;; figured out what it is exactly.
   (with-redefs [pom/add-classpath (constantly nil)]
-    (is (init-project
-         ;; we need to use a sequence of pairs rather than a map to
-         ;; reproduce the bug; IRL maps got converted to seqs somehow or
-         ;; another anyhow, but apparently not by init-project.
-         {:mirrors [["central" {:name "foo"
-                                :url "http://repo1.maven.org/maven2/"}]]
-          ;; Have to have a plugin to reproduce
-          :plugins '[[lein-pprint "1.1.1"]]}))))
+    (is (init-project project))))
