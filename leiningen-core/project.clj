1ec2d79fc911776aa5142b14c2fb36e9235f2137
Nelson Morris <nmorris@nelsonmorris.net>
2014-08-16 18:01:40 -0500

    Bump pedantic version
    I've redone some of the internals of the pedantic library, so it should
    be more efficent and has better documentation.
    
    Fixes #1491

--- leiningen-core/project.clj
+++ leiningen-core/project.clj
@@ -10,7 +10,7 @@
                  [com.cemerick/pomegranate "0.3.0"]
                  [org.apache.maven.wagon/wagon-http "2.4"]
                  [com.hypirion/io "0.3.1"]
-                 [pedantic "0.1.0"]]
+                 [pedantic "0.2.0"]]
   :scm {:dir ".."}
   :dev-resources-path "dev-resources"
   :aliases {"bootstrap" ["with-profile" "base"
