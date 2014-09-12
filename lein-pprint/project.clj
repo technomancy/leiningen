f2c07619718f4840b9a229bab6d0cff679aadcc1
Phil Hagelberg <technomancy@gmail.com>
2014-09-12 10:37:08 -0700

    Release lein-pprint 1.1.2, allows running outside projects.

--- lein-pprint/project.clj
+++ lein-pprint/project.clj
@@ -1,3 +1,6 @@
-(defproject lein-pprint "1.1.1"
+(defproject lein-pprint "1.1.2"
   :description "Pretty-print a representation of the project map."
+  :url "https://github.com/technomancy/leiningen"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
--- lein-pprint/src/leiningen/pprint.clj
+++ lein-pprint/src/leiningen/pprint.clj
@@ -1,7 +1,7 @@
 (ns leiningen.pprint
   (:require [clojure.pprint :as pprint]))
 
-(defn pprint
+(defn ^:no-project-needed pprint
   "Pretty-print a representation of the project map."
   [project & keys]
   (if (seq keys)
