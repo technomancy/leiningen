11c03643023386daef2274b04d22203fcfa119b3
Jean Niklas L'orange <jeannikl@hypirion.com>
2014-09-14 15:07:00 +0200

    Remove base dependencies for lein checkouts.
    Closes #1629.

--- project.clj
+++ project.clj
@@ -6,23 +6,24 @@
   :url "https://github.com/technomancy/leiningen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[leiningen-core "2.5.0-SNAPSHOT"]
-                 [org.clojure/data.xml "0.0.3"]
-                 [commons-io "2.4"]
-                 [bultitude "0.2.6"]
-                 [stencil "0.3.3" :exclusions [org.clojure/core.cache]]
-                 [org.apache.maven.indexer/indexer-core "4.1.3"
-                  :exclusions [org.apache.maven/maven-model
-                               org.sonatype.aether/aether-api
-                               org.sonatype.aether/aether-util
-                               org.sonatype.sisu/sisu-inject-plexus
-                               jakarta-regexp]]
-                 [reply "0.3.5" :exclusions [ring/ring-core
-                                             org.thnetos/cd-client]]
-                 ;; bump versions of various common transitive deps
-                 [slingshot "0.10.3"]
-                 [cheshire "5.0.2"]
-                 [clj-http "0.9.2" :exclusions [crouton]]]
+  :dependencies
+  ^:replace [[leiningen-core "2.5.0-SNAPSHOT"]
+             [org.clojure/data.xml "0.0.3"]
+             [commons-io "2.4"]
+             [bultitude "0.2.6"]
+             [stencil "0.3.3" :exclusions [org.clojure/core.cache]]
+             [org.apache.maven.indexer/indexer-core "4.1.3"
+              :exclusions [org.apache.maven/maven-model
+                           org.sonatype.aether/aether-api
+                           org.sonatype.aether/aether-util
+                           org.sonatype.sisu/sisu-inject-plexus
+                           jakarta-regexp]]
+             [reply "0.3.5" :exclusions [ring/ring-core
+                                         org.thnetos/cd-client]]
+             ;; bump versions of various common transitive deps
+             [slingshot "0.10.3"]
+             [cheshire "5.0.2"]
+             [clj-http "0.9.2" :exclusions [crouton]]]
   ;; checkout-deps don't work with :eval-in :leiningen
   :profiles {:dev {:resource-paths ["leiningen-core/dev-resources"]
                    :test-paths ["leiningen-core/test"]}
