403e96a51e1b8d9e83400018f3eac681022acd11
Chris Price <chris@puppetlabs.com>
2014-10-18 15:44:54 -0700

    (#1695) Respect priority when merging defaults
    This commit adds an extra condition at the beginning of the
    `setup-map-defaults` function, such that if one of the two
    items being merged has a higher priority than the other,
    it is honored.
    
    This seems to fix the issue where the `^:replace` metadata
    was not working properly

--- leiningen-core/src/leiningen/core/project.clj
+++ leiningen-core/src/leiningen/core/project.clj
@@ -319,7 +319,8 @@
      (fn [left right]
        ;; Assumes that left always contains :reduce OR :prepend in its meta
        (with-meta
-         (cond (-> left meta :reduce) (-> left meta :reduce
+         (cond (different-priority? left right) (pick-prioritized left right)
+               (-> left meta :reduce) (-> left meta :reduce
                                           (reduce left right))
                (-> left meta :prepend) (concat right left))
          (merge (meta left)
