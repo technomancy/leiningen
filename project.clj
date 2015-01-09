7aef5c3cfcfc68ee4762e27d623ecd1f93a4f2d5
Phil Hagelberg <phil@hagelb.org>
2015-01-09 09:55:40 -0800

    Bump version to 2.5.2-SNAPSHOT.
    [ci skip]

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 ;; This is Leiningen's own project configuration. See doc/TUTORIAL.md
 ;; file as well as sample.project.clj for help writing your own.
 
-(defproject leiningen "2.5.1"
+(defproject leiningen "2.5.2-SNAPSHOT"
   :description "Automate Clojure projects without setting your hair on fire."
   :url "https://github.com/technomancy/leiningen"
   :license {:name "Eclipse Public License"
