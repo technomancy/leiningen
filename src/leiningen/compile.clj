e2ee014dd14c39566de36aa8a53f0a6f037fb76e
B025994 <brandon.j.shimanek@gmail.com>
2015-09-09 16:47:54 -0500

    fixed printStackTrace bug using std out error

--- src/leiningen/compile.clj
+++ src/leiningen/compile.clj
@@ -139,7 +139,8 @@ Code that should run on startup belongs in a -main defn."
                      (try
                        (clojure.core/compile ~ns-sym)
                        (catch Throwable t#
-                         (.printStackTrace t#)
+                         (binding [*out* *err*]
+                           (.printStackTrace t#))
                          (throw t#))))
              project (update-in project [:prep-tasks]
                                 (partial remove #{"compile"}))]
