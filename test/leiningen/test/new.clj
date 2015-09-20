a8907c496d7688b203b604678c7c4e7b701b1984
Jean Niklas L'orange <jeannikl@hypirion.com>
2015-09-20 17:18:40 +0200

    Add CHANGELOG.md to tests.

--- test/leiningen/test/new.clj
+++ test/leiningen/test/new.clj
@@ -8,7 +8,7 @@
   (leiningen.new/new nil "test-new-proj")
   (is (= #{"README.md" "project.clj" "resources" "src" "core.clj" "test"
            "doc" "intro.md" "test_new_proj" "core_test.clj" ".gitignore" 
-           ".hgignore" "LICENSE"}
+           ".hgignore" "LICENSE" "CHANGELOG.md"}
          (set (map (memfn getName) (rest (file-seq (file "test-new-proj")))))))
   (delete-file-recursively (file "test-new-proj") :silently))
 
@@ -16,7 +16,7 @@
   (leiningen.new/new nil "orgname/a-project")
   (is (= #{"src" "a_project_test.clj" "project.clj" "a_project.clj" "orgname"
            "resources" "test" ".gitignore" "README.md" "doc" "intro.md"
-           "LICENSE" ".hgignore"}
+           "LICENSE" ".hgignore" "CHANGELOG.md"}
          (set (map (memfn getName)
                    (rest (file-seq (file "a-project")))))))
   (delete-file-recursively (file "a-project") :silently))
@@ -25,7 +25,7 @@
   (leiningen.new/new nil "default" "test-new-proj")
   (is (= #{"README.md" "project.clj" "src" "core.clj" "test" "resources"
            "doc" "intro.md" "test_new_proj" "core_test.clj" ".gitignore"
-           "LICENSE" ".hgignore"}
+           "LICENSE" ".hgignore" "CHANGELOG.md"}
          (set (map (memfn getName) (rest (file-seq (file "test-new-proj")))))))
   (delete-file-recursively (file "test-new-proj") :silently))
 
@@ -33,14 +33,14 @@
   (leiningen.new/new nil "app" "test-new-app")
   (is (= #{"README.md" "project.clj" "src" "core.clj" "test" "resources"
            "doc" "intro.md" "test_new_app" "core_test.clj" ".gitignore"
-           "LICENSE" ".hgignore"}
+           "LICENSE" ".hgignore" "CHANGELOG.md"}
          (set (map (memfn getName) (rest (file-seq (file "test-new-app")))))))
   (delete-file-recursively (file "test-new-app") :silently))
 
 (deftest test-new-with-plugin-template
   (leiningen.new/new nil "plugin" "test-new-plugin")
   (is (= #{"README.md" "project.clj" "src" "leiningen" "test_new_plugin.clj" ".gitignore"
-           "LICENSE" ".hgignore"}
+           "LICENSE" ".hgignore" "CHANGELOG.md"}
          (set (map (memfn getName) (rest (file-seq (file "test-new-plugin")))))))
   (delete-file-recursively (file "test-new-plugin") :silently))
 
@@ -48,7 +48,7 @@
   (leiningen.new/new nil "template" "test-new-template")
   (is (= #{"README.md" "project.clj" "src" "leiningen" "new" "resources"
            "test_new_template.clj" "test_new_template" "foo.clj" ".gitignore"
-           "LICENSE" ".hgignore"}
+           "LICENSE" ".hgignore" "CHANGELOG.md"}
          (set (map (memfn getName) (rest (file-seq (file "test-new-template")))))))
   (delete-file-recursively (file "test-new-template") :silently))
 
@@ -84,7 +84,7 @@
   (leiningen.new/new nil "test-new-proj" "--to-dir" "my-proj")
   (is (= #{"README.md" "project.clj" "src" "core.clj" "test" "resources"
            "doc" "intro.md" "test_new_proj" "core_test.clj" ".gitignore"
-           "LICENSE" ".hgignore"}
+           "LICENSE" ".hgignore" "CHANGELOG.md"}
          (set (map (memfn getName) (rest (file-seq (file "my-proj")))))))
   (delete-file-recursively (file "my-proj") :silently))
 
@@ -93,7 +93,7 @@
   (leiningen.new/new nil "test-new-proj" "--force")
   (is (= #{"README.md" "project.clj" "src" "core.clj" "test" "resources"
            "doc" "intro.md" "test_new_proj" "core_test.clj" ".gitignore"
-           "LICENSE" ".hgignore"}
+           "LICENSE" ".hgignore" "CHANGELOG.md"}
          (set (map (memfn getName) (rest (file-seq (file "test-new-proj")))))))
   (delete-file-recursively (file "test-new-proj") :silently))
 
@@ -102,7 +102,7 @@
   (leiningen.new/new nil "test-new-proj" "--to-dir" "my-proj" "--force")
   (is (= #{"README.md" "project.clj" "src" "core.clj" "test" "resources"
            "doc" "intro.md" "test_new_proj" "core_test.clj" ".gitignore"
-           "LICENSE" ".hgignore"}
+           "LICENSE" ".hgignore" "CHANGELOG.md"}
          (set (map (memfn getName) (rest (file-seq (file "my-proj")))))))
   (delete-file-recursively (file "my-proj") :silently))
 
@@ -117,7 +117,7 @@
     (leiningen.new/new nil "test-new-proj")
     (is (= #{"README.md" "project.clj" "src" "core.clj" "test" "resources"
              "doc" "intro.md" "test_new_proj" "core_test.clj" ".gitignore"
-             "LICENSE" ".hgignore"}
+             "LICENSE" ".hgignore" "CHANGELOG.md"}
            (set (map (memfn getName) (rest (file-seq (file new-pwd "test-new-proj")))))))
     (System/setProperty "leiningen.original.pwd" original-pwd)
     (delete-file-recursively (file new-pwd) :silently)))
