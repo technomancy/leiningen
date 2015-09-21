87c7e07da98c5be1f54db2ea176a0882175176b4
Jean Niklas L'orange <jeannikl@hypirion.com>
2015-09-22 00:37:56 +0200

    Back to snapshot

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 ;; This is Leiningen's own project configuration. See doc/TUTORIAL.md
 ;; file as well as sample.project.clj for help writing your own.
 
-(defproject leiningen "2.5.3"
+(defproject leiningen "2.6.0-SNAPSHOT"
   :description "Automate Clojure projects without setting your hair on fire."
   :url "https://github.com/technomancy/leiningen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[leiningen-core "2.5.3"]
+  :dependencies [[leiningen-core "2.6.0-SNAPSHOT"]
                  [org.clojure/data.xml "0.0.3"]
                  [commons-io "2.4"]
                  [bultitude "0.2.8"]
