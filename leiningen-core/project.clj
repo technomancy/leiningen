0fe3f86a3c832ef37d4abf56b44ba28599f0a360
Jean Niklas L'orange <jeannikl@hypirion.com>
2016-02-05 01:17:21 +0100

    Release 2.6.0

--- leiningen-core/project.clj
+++ leiningen-core/project.clj
@@ -1,4 +1,4 @@
-(defproject leiningen-core "2.6.0-SNAPSHOT"
+(defproject leiningen-core "2.6.0"
   :url "https://github.com/technomancy/leiningen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
