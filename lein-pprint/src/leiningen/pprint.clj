c63301a56432c4398b5ff86dc73ccb18d0b32c61
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2016-04-27 14:44:40 +0200

    Add support for get-in pprinting

--- lein-pprint/src/leiningen/pprint.clj
+++ lein-pprint/src/leiningen/pprint.clj
@@ -5,7 +5,10 @@
   "Pretty-print a representation of the project map."
   [project & keys]
   (if (seq keys)
-    (doseq [k keys]
-      (pprint/pprint (project (read-string k))))
+    (doseq [kstr keys]
+      (let [k (read-string kstr)]
+        (pprint/pprint (if (sequential? k)
+                         (get-in project k)
+                         (get project k)))))
     (pprint/pprint project))
   (flush))
