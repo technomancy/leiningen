3feae04e76a4d95d0ec55526b33694065c67ae9a
Chris Price <chris@puppetlabs.com>
2016-09-15 21:06:03 -0700

    Fix checking for SNAPSHOT deps with managed deps - fixes #2205
    This commit fixes #2205: when using managed dependencies in
    a project, if the project itself was a non-SNAPSHOT version,
    then when `lein install` and similar tasks were executed,
    lein would check to make sure that none of the dependencies
    were a SNAPSHOT version.  lein was checking that directly against
    the `:dependencies` vector, which would cause an NPE if a dependency
    didn't specify a version number (because it was inheriting it from
    the `:managed-dependencies` section).
    
    This commit fixes the bug by calling the code that merges the
    `:dependencies` and `:managed-dependencies` vectors together prior
    to performing the SNAPSHOT check.

--- test_projects/managed-deps-snapshot/project.clj
+++ test_projects/managed-deps-snapshot/project.clj
@@ -1,6 +1,6 @@
 (def clj-version "1.3.0")
 
-(defproject mgmt "0.99.0"
+(defproject mgmt "0.99.0-SNAPSHOT"
   :description "A test project"
 
   :managed-dependencies [[~(symbol "org.clojure" "clojure") ~clj-version]
