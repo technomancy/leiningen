c06bc76959ab3377fab4865f6acce1e08bd8fdbc
Jean Niklas L'orange <jeannikl@hypirion.com>
2016-09-22 02:48:48 +0200

    Back to snapshot

--- bin/lein.bat
+++ bin/lein.bat
@@ -2,7 +2,7 @@
 
 setLocal EnableExtensions EnableDelayedExpansion
 
-set LEIN_VERSION=2.7.1
+set LEIN_VERSION=2.7.2-SNAPSHOT
 
 if "%LEIN_VERSION:~-9%" == "-SNAPSHOT" (
     set SNAPSHOT=YES
--- bin/lein.ps1
+++ bin/lein.ps1
@@ -34,7 +34,7 @@ function Set-ParentLocation([string]$file)
 
 function Initialize-Environment
 {
-    $env:LEIN_VERSION = '2.7.1'
+    $env:LEIN_VERSION = '2.7.2-SNAPSHOT'
     $env:SNAPSHOT = if($env:LEIN_VERSION -like '*-SNAPSHOT'){'YES'}else{'NO'} #TODO: Still needed?
     $env:ORIGINAL_PWD = $PWD -replace '\\$','\\'
     Set-ParentLocation project.clj
