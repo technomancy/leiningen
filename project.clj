14c7f1ecc5b1b9d973d4ab40893bb835edbf44c9
Phil Hagelberg <phil@hagelb.org>
2017-04-25 13:47:04 -0700

    Add back in commons-lang dependency.

--- project.clj
+++ project.clj
@@ -7,10 +7,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[leiningen-core "2.7.2-SNAPSHOT"]
-                 [org.clojure/data.xml "0.0.8"] ; needed for pom
-                 [bultitude "0.2.8"] ; needed for test
-                 [stencil "0.5.0" :exclusions [org.clojure/core.cache]] ; needed for new
-                 [org.clojure/tools.nrepl "0.2.12"] ; for repl, obviously
+                 ;; needed for pom
+                 [org.clojure/data.xml "0.0.8"]
+                 ;; needed for test
+                 [bultitude "0.2.8"]
+                 ;; needed for new
+                 [stencil "0.5.0" :exclusions [org.clojure/core.cache]]
+                 ;; needed for uberjar
+                 [commons-lang "2.6"]
+                 ;; needed for repl
+                 [org.clojure/tools.nrepl "0.2.12"]
                  ;; needed for change
                  [net.cgrand/sjacket "0.1.1" :exclusions [org.clojure/clojure]]
                  ;; bump versions of various common transitive deps
