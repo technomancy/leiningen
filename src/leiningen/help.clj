319d907a6116d0132c315fd21b2584a16cb45fda
irinarenteria <renteria.irina@gmail.com>
2017-09-17 09:54:54 -0500

    Check presence of substring using includes

--- src/leiningen/help.clj
+++ src/leiningen/help.clj
@@ -70,10 +70,10 @@
   "Returns a string containing help content. Removes doctoc comments if they
   are present."
   [help-text]
-   (let [doctoc-text #"<!-- END doctoc generated TOC please keep comment here to allow auto update -->"]
-     (if (boolean (re-find doctoc-text help-text))
-       (string/triml (second (string/split help-text doctoc-text))))
-       help-text))
+   (let [doctoc-text "<!-- END doctoc generated TOC please keep comment here to allow auto update -->"]
+     (if (string/includes? help-text doctoc-text)
+       (string/triml (second (string/split help-text (re-pattern doctoc-text))))
+       help-text)))
 
 (defn- static-help [name]
   (if-let [resource (io/resource (format "leiningen/help/%s" name))]
