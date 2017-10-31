28994c34b258a40d87b02fcec07241b0ad98095b
Erik Assum <erik@assum.net>
2017-10-31 21:23:56 +0100

    Don't use :main, use :repl-options

--- resources/leiningen/new/default/project.clj
+++ resources/leiningen/new/default/project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main {{namespace}})
+  :repl-options {:init-ns {{namespace}}})
