e20c06bf8535078ebcf2f242833a1c61c49c6f79
Phil Hagelberg <phil@hagelb.org>
2017-12-15 15:47:51 -0800

    Bump templates to use Clojure 1.9.0.
    Fixes #2373.

--- resources/leiningen/new/app/project.clj
+++ resources/leiningen/new/app/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]]
   :main ^:skip-aot {{namespace}}
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
