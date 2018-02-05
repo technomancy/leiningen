7990ca1f97201715df8d41f67f99f1b6ebf9338a
WillChilds-Klein <willck93@gmail.com>
2018-02-04 19:34:39 -0500

    Bump reply to 0.3.8
    closes #1390

--- src/leiningen/repl.clj
+++ src/leiningen/repl.clj
@@ -198,7 +198,7 @@
 
 (def reply-profile
   {:dependencies
-   '[^:displace [reply "0.3.7"
+   '[^:displace [reply "0.3.8"
                  :exclusions [org.clojure/clojure ring/ring-core]]
      [clojure-complete "0.2.4"]]})
 
