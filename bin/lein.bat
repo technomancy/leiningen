b119639caa09458f32d9d17e09bfa289a9bcaa1e
Phil Hagelberg <phil@hagelb.org>
2019-02-10 16:34:59 -0800

    Bump version to 2.9.1-SNAPSHOT.

--- bin/lein.bat
+++ bin/lein.bat
@@ -2,7 +2,7 @@
 
 setLocal EnableExtensions EnableDelayedExpansion
 
-set LEIN_VERSION=2.9.0
+set LEIN_VERSION=2.9.1-SNAPSHOT
 
 if "%LEIN_VERSION:~-9%" == "-SNAPSHOT" (
     set SNAPSHOT=YES
--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 ;; This is Leiningen's own project configuration. See doc/TUTORIAL.md
 ;; file as well as sample.project.clj for help writing your own.
 
-(defproject leiningen "2.9.0"
+(defproject leiningen "2.9.1-SNAPSHOT"
   :description "Automate Clojure projects without setting your hair on fire."
   :url "https://github.com/technomancy/leiningen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   ;; If you update these, update resources/leiningen/bootclasspath-deps.clj too
-  :dependencies [[leiningen-core "2.9.0"]
+  :dependencies [[leiningen-core "2.9.1-SNAPSHOT"]
                  ;; needed for pom
                  [org.clojure/data.xml "0.0.8"]
                  ;; needed for test
