b59ef04c3487c350201316b8b30a87d3c6beac46
Rob Hanlon <robhanlon@gmail.com>
2020-01-31 19:13:44 -0800

    Fix broken pprint plugin

--- lein-pprint/src/leiningen/pprint.clj
+++ lein-pprint/src/leiningen/pprint.clj
@@ -1,25 +1,20 @@
 (ns leiningen.pprint
-  (:require [clojure.pprint :as pprint]
-            [leiningen.core :refer [abort]]))
+  (:require [clojure.pprint :as pprint]))
 
 (defn ^:no-project-needed pprint
-  "Usage: pprint [--not-pretty] [--] [selector...]
+  "Usage: pprint [--no-pretty] [--] [selector...]
 
   When no selectors are specified, pretty-prints a representation of
   the entire project map.  Otherwise pretty-prints the item(s)
   retrieved by (get-in project selector) when reading a selector
   produces something sequential, or (get project selector) when it
-  doesn't.  If \"--not-pretty\" is specified, doesn't pretty-print,
+  doesn't.  If \"--no-pretty\" is specified, doesn't pretty-print,
   just prints."
   [project & keys]
-  (let [[pretty? keys] (loop [args args
-                              pretty? true]
-                         (if-let [[arg & args] (seq args)]
-                           (case arg
-                             "--" [pretty? args]
-                             "--no-pretty" (recur false args)
-                             (abort "Unrecognized argument" (pr-str arg)))))
-        show (if pretty? pprint/pprint println)]
+  (let [[args rest] (split-at 2 keys)
+        [show keys] (if (= args ["--no-pretty" "--"])
+                      [println rest]
+                      [pprint/pprint keys])]
     (if (seq keys)
       (doseq [kstr keys]
         (let [k (read-string kstr)]
--- lein-pprint/test/leiningen/pprint_test.clj
+++ lein-pprint/test/leiningen/pprint_test.clj
@@ -2,6 +2,18 @@
   (:require [clojure.test :refer :all]
             [leiningen.pprint :refer :all]))
 
-(deftest can-pprint
-  (is (= "1\n" (with-out-str (pprint {:foo 1} ":foo"))))
-  (is (= "1\n" (with-out-str (pprint {:foo {:bar 1}} "[:foo :bar]")))))
+(def shallow {:foo "str"})
+(def deep {:foo {:bar "str"}})
+
+(deftest can-pprint-shallow
+  (is (= "\"str\"\n" (with-out-str (pprint shallow ":foo")))))
+
+(deftest can-pprint-deep
+
+  (is (= "\"str\"\n" (with-out-str (pprint deep "[:foo :bar]")))))
+
+(deftest can-println-shallow
+  (is (= "str\n" (with-out-str (pprint shallow "--no-pretty" "--" ":foo")))))
+
+(deftest can-println-deep
+  (is (= "str\n" (with-out-str (pprint deep "--no-pretty" "--" "[:foo :bar]")))))
