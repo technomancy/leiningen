3af6991565f8003366f49f2b6d1d01c9fd27b6b5
ben.sless <ben.sless@appsflyer.com>
2020-05-29 19:09:18 +0300

    Add direct-linking jvm-opt to app uberjar profile
    Closes #2684

--- resources/leiningen/new/app/project.clj
+++ resources/leiningen/new/app/project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot {{namespace}}
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
