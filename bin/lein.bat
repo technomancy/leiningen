f1921a55ae83c419ef4528f92c65a01f48603b80
Phil Hagelberg <phil@hagelb.org>
2021-09-15 09:52:27 -0700

    Bump to next snapshot version and automate bumping.

--- bin/lein.bat
+++ bin/lein.bat
@@ -2,7 +2,7 @@
 
 setLocal EnableExtensions EnableDelayedExpansion
 
-set LEIN_VERSION=2.9.7
+set LEIN_VERSION=2.9.8-SNAPSHOT
 
 if "%LEIN_VERSION:~-9%" == "-SNAPSHOT" (
     set SNAPSHOT=YES
--- leiningen-core/project.clj
+++ leiningen-core/project.clj
@@ -1,4 +1,4 @@
-(defproject leiningen-core "2.9.7"
+(defproject leiningen-core "2.9.8-SNAPSHOT"
   :url "https://github.com/technomancy/leiningen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
