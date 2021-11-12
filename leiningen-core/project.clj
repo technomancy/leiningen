05a53d1a9410e972120f6afec6432b43aaf977ad
Phil Hagelberg <phil@hagelb.org>
2021-11-11 19:27:02 -0800

    Bump to 2.9.9-SNAPSHOT

--- leiningen-core/project.clj
+++ leiningen-core/project.clj
@@ -1,4 +1,4 @@
-(defproject leiningen-core "2.9.8"
+(defproject leiningen-core "2.9.9-SNAPSHOT"
   :url "https://github.com/technomancy/leiningen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
