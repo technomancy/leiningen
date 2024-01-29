d5647f26332358ff80edf28f81714fe898af0809
Phil Hagelberg <phil@hagelb.org>
2024-01-28 20:43:31 -0800

    Release 2.11.1

--- leiningen-core/project.clj
+++ leiningen-core/project.clj
@@ -1,4 +1,4 @@
-(defproject leiningen-core "2.11.1-SNAPSHOT"
+(defproject leiningen-core "2.11.1"
   :url "https://codeberg.org/leiningen/leiningen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
