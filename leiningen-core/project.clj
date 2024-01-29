516ec23c703853064f997404c5f9ea7511bf8a81
Phil Hagelberg <phil@hagelb.org>
2024-01-28 20:55:10 -0800

    Bump to 2.11.2-SNAPSHOT

--- leiningen-core/project.clj
+++ leiningen-core/project.clj
@@ -1,4 +1,4 @@
-(defproject leiningen-core "2.11.1"
+(defproject leiningen-core "2.11.2-SNAPSHOT"
   :url "https://codeberg.org/leiningen/leiningen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
